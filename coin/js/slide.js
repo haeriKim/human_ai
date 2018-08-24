7f382a9a633e079ab5859d4807c9c263572611d7
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-08-24 17:33:01 +0900

    전현석 // 공지사항 마크업, 슬라이드css수정

---
+++ coin/js/slide.js
@@ -0,0 +1,36 @@
+/*slide 슬라이드*/
+
+$(document).ready(function(){
+  var nowNum = 0;
+  var st;
+  var bannerObj = $('.slideUl li');
+  var maxLength = bannerObj.length;
+  var timer = 7000;
+  var autoRun = setInterval(function(){rollingFn('right');},timer);
+  bannerObj.eq(nowNum).fadeIn();
+  $('.nextBtn').on('click',function(){
+    rollingFn('right');
+  });
+  $('.prevBtn').on('click',function(){
+    rollingFn('left');
+  });
+
+  function rollingFn(direction){
+    if(direction=='right'){
+      nowNum = nowNum+1;
+      if(nowNum>maxLength-1){
+        nowNum = 0;
+      }
+    }else{
+      nowNum = nowNum-1;
+      if(nowNum<0){
+        nowNum = maxLength-1;
+      }
+    }
+    bannerObj.hide().eq(nowNum).fadeIn();
+  }
+
+});
+/*  $('.banner').hover(function(){
+    $(this).stop()
+}); */
