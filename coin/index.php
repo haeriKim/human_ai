da9951da852a53765eebf3a18f3c3c92978060d9
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-09-12 16:51:12 +0900

    전현석 // index 책갈피메뉴

--- coin/index.php
+++ coin/index.php
@@ -184,6 +184,12 @@
                         <ul>
                           <li>
                             <a href="#">
+                              <img src="img/security_center.png" alt="코인정보">
+                              <p>보안센터</p>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="#">
                               <img src="img/coin-inform.png" alt="코인정보">
                               <p>코인정보</p>
                             </a>
@@ -206,9 +212,120 @@
                 </div><!-- main_content End-->
             </div><!-- content_wrap End-->
 
+
             <!-- footer_wrap -->
             <?php include "footer.php";?>
             <!-- footer_wrap End-->
+
+            <!--submenu_mypage-->
+            <div class="submenu_mypage">
+              <a href="#"><img src="img/mypage.png" alt="마이페이지"></a>
+            </div>
+            <!--submenu_mypage End-->
+
+            <!--submenu_mypage_inner-->
+            <div class="box submenu_mypage_inner">
+
+                <ul class="profile">
+                  <li id="profile_name">가*다 님</li>
+                  <li>abc@naver.com</li>
+                </ul>
+                    <img src="img/p_next.png" alt="뒤로가기" class="submenu_mypage_back">
+
+
+                    <div class="submenu_mypage_info">
+                      <select name="코인선택" id="submenu_mypage_select">
+                        <option value="원화">원화</option>
+                        <option value="비트코인">비트코인</option>
+                        <option value="비트코인캐시">비트코인캐시</option>
+                        <option value="이더리움">이더리움</option>
+                        <option value="이더리움클래식">이더리움클래식</option>
+                        <option value="리플">리플</option>
+                        <option value="에이다">에이다</option>
+                        <option value="이오스">이오스</option>
+                      </select>
+
+                        <table class="submenu_mypage_table">
+                          <tr>
+                            <th>원화잔고</th>
+                            <td>125,000,000 <span>KRW</span></td>
+                          </tr>
+
+                          <tr>
+                            <th>총 평가금액</th>
+                            <td>7,56,000,000 <span>KRW</span></td>
+                          </tr>
+
+                          <tr>
+                            <th>총 보유자산</th>
+                            <td>1,050,000,000 <span>KRW</span></td>
+                          </tr>
+
+                          <tr>
+                            <th>총 매수금액</th>
+                            <td>1,000 <span>KRW</span></td>
+                          </tr>
+
+                          <tr>
+                            <th>총 평가손익</th>
+                            <td>125,000 <span>KRW</span></td>
+                          </tr>
+
+                          <tr>
+                            <th>총 수익률</th>
+                            <td>189,000,000 <span>KRW</span></td>
+                          </tr>
+
+                        </table>
+
+                      <div class="submenu_mypage_table_bottom">
+                        <span>가*다 님은 현재 보안등급 <b>2단계</b>입니다.</span>
+                        <p><a href="certification.php">인증하기</a></p>
+
+                      </div>
+                  </div>
+            </div><!--submenu_mypage_inner-->
+
+            <script>
+              $(".submenu_mypage").click(function(){
+                $(".submenu_mypage_inner").stop().animate({right:0},300);
+              });
+
+              $(".submenu_mypage_back").click(function(){
+                $(".submenu_mypage_inner").stop().animate({right:-300},300);
+              });
+            </script>
+
+            <!--submenu_security-->
+            <div class="submenu_security">
+              <a href="#"><img src="img/security.png" alt="보안페이지"></a>
+            </div>
+            <!--submenu_security End-->
+
+            <!--submenu_security_inner-->
+            <div class="box submenu_security_inner">
+              <img src="img/p_next.png" alt="뒤로가기" class="submenu_mypage_back2">
+              <div class="submenu_security_title">
+                <p>Human & AI</p>
+                <p>Si Security System</p>
+              </div>
+
+              <div class="submenu_security_story">
+                <p>안전한 거래를 위해 CCE는</p>
+                <p>SI Security System을 가동하고 있습니다.</p>
+              </div>
+
+            </div><!--submenu_security_inner-->
+
+            <script>
+              $(".submenu_security").click(function(){
+                $(".submenu_security_inner").stop().animate({right:0},300);
+              });
+
+              $(".submenu_mypage_back2").click(function(){
+                $(".submenu_security_inner").stop().animate({right:-300},300);
+              });
+            </script>
 
         </div><!-- container End -->
     </div><!-- wrap End -->
