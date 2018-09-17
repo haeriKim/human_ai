c261585f3010bb7bfd1693edd101a7ddeff266bd
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-09-17 15:33:03 +0900

    서진웅 // 수정

--- coin/js/navi_slider.js
+++ coin/js/navi_slider.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
     $('.content_nav ul li dl.off').hide();
     $('.content_nav ul li a.more_menu').click(function(){
         $(this).parent().toggleClass('on');
-        $(this).next().slideToggle();
+        $(this).next().stop().slideToggle();
     })
 })
