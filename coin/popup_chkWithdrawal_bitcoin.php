de62f90472bf8d370acf9fb4d8788e99e44844d6
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-09-21 19:58:32 +0900

    서진웅 // 코인 출금 확인 팝업창

---
+++ coin/popup_chkWithdrawal_bitcoin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>CCE</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/popup.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+</head>
+<body>
+    <div id="full">
+            <!-- full_content -->
+            <div id="popupbox" class='box popup coin_withdrawal_box'>
+                <div class="full_inner">
+                    <form class="" action="#" method="post">
+                        <h2>출금신청</h2>
+                        <p><b class='check_your_money_again'><span class="withdrawal_price">20 </span> BTC</b>를</p>
+                        <table class='left_thick_table'>
+                            <tbody>
+                                <tr>
+                                    <th>나의 비트코인 주소</th>
+                                    <td>
+                                        <span>ewearch&sm=top_hty&fbm=1%ie-itf8quera</span>
+                                        <div class='plus_more_btn plus_more_btn_01'>
+                                            <img src="img/plus.png" alt="자세히보기">
+                                            <div class='more_coin_address_01'>ewearch&sm=top_hty&fbm=1%ie-itf8query1664=</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>비트코인 출금 주소</th>
+                                    <td>
+                                        <span>asdlkjlqkwasdasdeli@oasdasd1231234asdsd</span>
+                                        <div class='plus_more_btn plus_more_btn_02'>
+                                            <img src="img/plus.png" alt="자세히보기">
+                                            <div class='more_coin_address_02'>asdlkjlqkwasdasdeli@oasdasd1231234asdsdwsasd</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <p>현재 비트코인 지갑에서 출금하시겠습니까?</p>
+                        <div class="cancel_or_apply">
+                            <input type="reset" name="" value="취소" class='cancel_btn'>
+                            <input type="submit" name="" value="출금신청" class='agree_btn' onclick="location.href='mywallet_output_complete_KRW.php'">
+                        </div>
+                    </form>
+                </div>
+            </div><!-- full_content End-->
+            <div id="shadow"></div>
+    </div>  <!-- full End-->
+</body>
+</html>
