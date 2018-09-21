3be08eb4581e4068f1c462e21a2ff6384bc6e3ef
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-09-21 19:00:58 +0900

    김해리 // 주소 복사 팝업 추가

---
+++ coin/popup_adrCopy.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="ko">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1">
+  <title>출금 계좌 등록</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/common.css">
+  <link rel="stylesheet" href="css/popup.css">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/common.js"></script>
+
+
+ </head>
+ <body>
+    <div id="full">
+            <!-- full_content -->
+            <div id="popupbox" class='box popup copy_pop'>
+                <div class="full_inner">
+                    <h2>복사 완료</h2>
+                    <div class="popupbox_text">
+                      <p>주소가 복사되었습니다.</p>
+                      <b>
+                        <img src="img/icon_ex.png" alt="주의 아이콘"/>
+                        주소를 붙여넣기를 하신 후,<br />
+                        현재 복사 된 주소와 동일한지 다시 한번 꼭 확인해주세요.
+                      </b>
+                    </div>
+                    <div class="btn_area">
+                      <input type="button" name="" value="닫기" class="popupbt ok"/>
+                    </div>
+                </div>
+            </div><!-- full_content End-->
+            <div id="shadow"></div>
+    </div>  <!-- full End-->
+ </body>
+ </html>
