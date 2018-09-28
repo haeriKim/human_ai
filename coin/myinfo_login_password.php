df66bd74fb9bb93cf325ce046f8c224958aacca0
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-09-28 18:09:24 +0900

    전현석 // 로그인 비밀번호 변경 링크

--- coin/myinfo_login_password.php
+++ coin/myinfo_login_password.php
@@ -155,8 +155,8 @@ jQuery(document).ready(function(){
 
                             </div><!-- myinfo_table_wrap End-->
                             <div class="cancel_or_apply">
-                              <input type="reset" name="reset" value="취소" onclick="location.href='myinfo.php'"class="cancel_btn">
-                              <input type="submit" name="submit" value="변경" class="agree_btn if_you_all_agree">
+                              <input type="reset" name="reset" value="취소" onclick="location.href='myinfo.php'" class="cancel_btn">
+                              <input type="submit" name="submit" value="변경" onclick="location.href='myinfo_login_password_success.php'" class="agree_btn if_you_all_agree">
                             </div>
                           </form>
                         </div><!-- myinfo_sub_box End-->
