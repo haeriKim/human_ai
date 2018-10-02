4e722bd32d15a8656c0cb18df6002efefc681ab3
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-10-02 14:05:53 +0900

    김해리 // 메인,로그인 팝업 ui 수정

--- coin/popup_login.php
+++ coin/popup_login.php
@@ -18,14 +18,14 @@
            <!-- full_content -->
            <div id="popupbox" class='box popup'>
                <div class="full_inner">
-                   <h2>로그인 하기</h2>
+                   <h2>로그인하기</h2>
                    <div class="popupbox_text">
                      <p>로그인이 필요한 서비스입니다.</p>
                      <p>로그인하시겠습니까?</p>
                    </div>
                    <div class="btn_area">
                      <input type="button" name="cancel" value="확인" class="popupbt">
-                     <input type="button" name="submit" value="로그인하러가기" class="popupbt ok" onclick="location.href='login.php'">
+                     <input type="button" name="submit" value="로그인하기" class="popupbt ok" onclick="location.href='login.php'">
                    </div>
                    <a href="signUp.php" class="join_btn">아직 The Alls 계정이 없으시다면?</a>
                </div>
