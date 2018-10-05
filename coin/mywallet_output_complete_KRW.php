dd8bced0f5a167fd2480cc4c399e68795e60a59d
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-10-05 12:23:17 +0900

    김해리 // 원화 입출금, 코인 출금 완료 페이지

--- coin/mywallet_output_complete_KRW.php
+++ coin/mywallet_output_complete_KRW.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/mywallet_output_complete_KRW.css">
+    <link rel="stylesheet" href="css/mywallet_complete.css">
     <link rel="stylesheet" href="css/sub_menu.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <script src="js/jquery-3.2.1.min.js"></script>
@@ -34,201 +34,54 @@
             <!-- content_wrap -->
             <div class="content_wrap">
                 <!-- main_content -->
-                <div class="content mycoin_content con_1340">
-                      <div class="box con_410" id='mywallet_havecoin'>
-                          <h3 class='mywallet_h3'>보유코인</h3>
+                <div class="content mycoin_c_content">
+                      <!-- con_1000 -->
+                    <div class="box con_1000">
+                      <!-- output_KRW_complete -->
+                      <div id='output_KRW_complete' class='mywallet_complete KRW_complete'>
+                          <h3>원화(KRW) 출금</h3>
+                          <p class='complete_paragraph'>출금신청이 완료되었습니다.</p>
+
                           <form class="" action="#" method="post">
-                              <input type="checkbox" id='have_coin' name='have_coin'>
-                              <label for='have_coin'>보유코인</label>
+                              <!-- account_table_wrap -->
+                              <div class="account_table_wrap">
+                                  <!-- send_account -->
+                                  <div class="send_account">
+                                      <p>출금 정보</p>
+                                      <!-- left_thick_table -->
+                                      <table class='left_thick_table'>
+                                          <tbody>
+                                              <tr>
+                                                  <th>은행</th>
+                                                  <td><span class='bankName'>○○은행</span></td>
+                                              </tr>
+                                              <tr>
+                                                  <th>계좌번호</th>
+                                                  <td><span class='user_account'>1234-567-8910-11-123</span></td>
+                                              </tr>
+                                              <tr>
+                                                  <th>예금주</th>
+                                                  <td><span class='user_name'>가나다</span></td>
+                                              </tr>
+                                              <tr>
+                                                  <th>출금날짜</th>
+                                                  <td>2018-09-06 19:06:05</td>
+                                              </tr>
+                                          </tbody>
+                                      </table><!-- left_thick_table End-->
+                                  </div><!-- send_account End-->
+                              </div><!-- account_table_wrap End-->
+
+                              <div class="send_info_01">
+                                  <p class='input_money'>출금금액</p>
+                                  <p class='money_account'>11,000,000<span>KRW</span></p>
+                              </div>
+                              <div class="button_group">
+                                  <input type="submit" name="submit" value="입출금 내역보기" class='look_input_output_list button_style'/>
+                              </div>
                           </form>
-                          <!-- deal_table_scroll -->
-                          <div class="deal_table_scroll">
-                              <table class='deal_table' summary='코인명,보유비중,보유수량,상태 정보를 알 수 있는 창'>
-                                <colgroup>
-                                    <col width='37%'/>
-                                    <col width='16%'/>
-                                    <col width='37%'/>
-                                    <col width='10%'/>
-                                </colgroup>
-                                <thead class='thead_deco mywallet_havecoin_thead'>
-                                    <tr>
-                                        <th class='coin_name'>코인명</th>
-                                        <th class='coin_portion'>보유비중</th>
-                                        <th class='coin_amount'>보유수량</th>
-                                        <th class='coin_condition'>상태</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class='active'>
-                                        <td class='coin_kor'>
-                                            <img src="img/won.png" alt="원화아이콘">
-                                            <p class='style_bold'>원화</p>
-                                            <span class='unit'>KRW</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>100.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>750,000,000<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href='#' class='button_hover'>KRW변환</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/bitcoin.png" alt="비트코인아이콘">
-                                            <p class='style_bold'>비트코인</p>
-                                            <span class='unit'>BTC</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>BTC</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/bitcoincash.png" alt="비트코인캐시아이콘">
-                                            <p class='style_bold'>비트코인캐시</p>
-                                            <span class='unit'>BCH</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>BCH</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>출금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/eth.png" alt="이더리움아이콘">
-                                            <p class='style_bold'>이더리움</p>
-                                            <span class='unit'>ETH</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>ETH</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입출금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/etc.png" alt="이더리움클래식아이콘">
-                                            <p class='style_bold'>이더리움클래식</p>
-                                            <span class='unit'>ETC</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>ETC</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입출금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/reaple.png" alt="리플아이콘">
-                                            <p class='style_bold'>리플</p>
-                                            <span class='unit'>XRP</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>XRP</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입출금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/ada.png" alt="에이다아이콘">
-                                            <p class='style_bold'>에이다</p>
-                                            <span class='unit'>ADA</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>ADA</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입출금</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td class='coin_kor'>
-                                            <img src="img/eos.png" alt="이오스아이콘">
-                                            <p class='style_bold'>이오스</p>
-                                            <span class='unit'>EOS</span>
-                                        </td>
-                                        <td class='amount_content'><span class='up_down_data style_bold'>0.00%</span></td>
-                                        <td class='have_coin_amount'>
-                                            <p class='up_down_data style_bold'>0.00<span class='unit_style'>EOS</span></p>
-                                            <p class='up_down_data'>0<span class='unit_style'>KRW</span></p>
-                                        </td>
-                                        <td><a href="#" class='button_hover'>입출금</a></td>
-                                    </tr>
-                                </tbody>
-                              </table>
-                          </div><!-- deal_table_scroll End-->
-                      </div><!-- con_410 End-->
-
-                      <!-- mycoin_fence -->
-                    <div class="box mycoin_fence">
-                  <!--coin탭태두리-->
-                    <div class="mycoin_tab" id="mycoin_tab">
-                        <ul class="tabs">
-                            <li rel="tab1">보유코인</li>
-                            <li rel="tab2">거래내역</li>
-                            <li class="active" rel="tab3">입출금</li>
-                        </ul>
-                    </div><!--coin탭태두리 End-->
-
-                    <!--mycointab_container-->
-                            <div class="mycointab_container">
-                                    <!-- #tab3 입출금-->
-                                    <div id="tab3" class="tab_content">
-                                        <!-- input_KRW_complete -->
-                                        <div id='input_KRW_complete' class='input_KRW_complete'>
-                                            <p class='complete_paragraph'>출금신청이 완료되었습니다.</p>
-
-                                            <form class="" action="#" method="post">
-                                                <!-- account_table_wrap -->
-                                                <div class="account_table_wrap">
-                                                    <!-- send_account -->
-                                                    <div class="send_account">
-                                                        <p>출금될 은행 계좌정보</p>
-                                                        <!-- left_thick_table -->
-                                                        <table class='left_thick_table'>
-                                                            <tbody>
-                                                                <tr>
-                                                                    <th>은행</th>
-                                                                    <td><span class='bankName'>○○은행</span></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <th>계좌번호</th>
-                                                                    <td><span class='user_account'>1234-567-8910-11-123</span></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <th>예금주</th>
-                                                                    <td><span class='user_name'>가나다</span></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <th>출금날짜</th>
-                                                                    <td>2018-09-06 19:06:05</td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table><!-- left_thick_table End-->
-                                                    </div><!-- send_account End-->
-                                                </div><!-- account_table_wrap End-->
-
-                                                <div class="send_info_01">
-                                                    <p class='input_money'>출금금액</p>
-                                                    <p class='money_account'>11,000,000<span>KRW</span></p>
-                                                </div>
-                                                <div class="button_group">
-                                                    <input type="submit" name="submit" value="출금 내역보기" class='look_input_output_list button_style'/>
-                                                </div>
-                                            </form>
-                                        </div><!-- input_KRW_complete End-->
-
-                                    </div><!--#tab3 입출금 End-->
-                               </div><!--mycointab_container End-->
-                        </div>  <!-- mycoin_fence End-->
+                      </div><!-- output_KRW_complete End-->
+                        </div>  <!-- con_1000 End-->
                   </div><!-- mycoin_content End-->
               </div><!-- content_wrap End-->
 
