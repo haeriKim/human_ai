4abb0169245d004e4970c1d740d305e607190e73
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-10 17:43:23 +0900

    전현석 // 1:1문의 자세히보기

--- coin/customer_questionList.php
+++ coin/customer_questionList.php
@@ -110,7 +110,7 @@
                                     <tr>
                                       <td><span class="number">5</td>
                                       <td>입출금</td>
-                                      <td id="left_text"><a href="customer_question_detail.php">입출금 문의관련에 대해 문의 드립니다.</a></td>
+                                      <td id="left_text"><a href="customer_question_detail.php">암호화폐 송금 주소를 잘못 입력했어요.</a></td>
                                       <td>2018-10-01</td>
                                       <td class="delay">대기중</td>
                                     </tr>
