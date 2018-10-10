0a35d4784c6d38ece305af1b236c81bb2d9f430d
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-10-10 12:19:10 +0900

    김해리 // 비밀번호변경 플홀 변경

--- coin/login_changePw.php
+++ coin/login_changePw.php
@@ -40,21 +40,21 @@
                         <form class="" action="#" method="post">
                             <h3>비밀번호 변경</h3>
                             <label for="password" class='skip_label'>비밀번호</label>
-                            <input type="password" name="password" value="" placeholder="비밀번호" id='password' required autocomplete="off" class='user_pass'>
+                            <input type="password" name="password" value="" placeholder="새로운 로그인 비밀번호를 입력하세요." id='password' required autocomplete="off" class='user_pass'>
                             <p>비밀번호 요건이 충족하지 않습니다.</p>
                             <div class="question_smallbox"><img src="img/question.png">
                               <div class="password_question_detail">
                                     안전한 비밀번호 작성 법<br />
-                                    -비밀번호는 영문 소문자, 대문자, 숫자, 특수문자를 포함하여 10자리 이상으로 만드셔야 합니다. <br />
-                                    -비밀번호는 최대한 길게 작성해주세요.<br />
-                                    -단, 허용되는 특수문자(~!@#$%^*)외 다른 특수문자는 사용할 수 없습니다.<br />
-                                    -ID, 생일, 전화번호 등 개인정보를 이용해 만든 비밀번호를 사용하지 마세요.<br />
-                                    -포털 사이트나 타 거래소 등 타 사이트와 동일하거나 비슷한 암호를 설정하지 마세요.타 사이트에서 암호가 유출될 경우 제3자가 회원님의 계정에 접근할 위험이 있습니다.<br />
-                                    -동일한 문자가 반복되는 등 패턴이 있는 비밀번호로 만들지 마세요.<br />
+                                    - 비밀번호는 영문 소문자, 대문자, 숫자, 특수문자를 포함하여 10자리 이상으로 만드셔야 합니다. <br />
+                                    - 비밀번호는 최대한 길게 작성해주세요.<br />
+                                    - 단, 허용되는 특수문자(~!@#$%^*)외 다른 특수문자는 사용할 수 없습니다.<br />
+                                    - ID, 생일, 전화번호 등 개인정보를 이용해 만든 비밀번호를 사용하지 마세요.<br />
+                                    - 포털 사이트나 타 거래소 등 타 사이트와 동일하거나 비슷한 암호를 설정하지 마세요.타 사이트에서 암호가 유출될 경우 제3자가 회원님의 계정에 접근할 위험이 있습니다.<br />
+                                    - 동일한 문자가 반복되는 등 패턴이 있는 비밀번호로 만들지 마세요.<br />
                               </div>
                             </div>
                             <label for="password_check" class='skip_label'>비밀번호 확인</label>
-                            <input type="password" name="password_check" value="" placeholder="비밀번호 확인" id='password_check' required autocomplete="off" class='user_passChk'>
+                            <input type="password" name="password_check" value="" placeholder="새로운 로그인 비밀번호를 다시 입력하세요." id='password_check' required autocomplete="off" class='user_passChk'>
                             <p>비밀번호가 일치하지 않습니다.</p>
                             <input type="submit" name="비밀번호 변경" value="비밀번호 변경" class='button_box change_Pw_button'  onclick="location.href='login_c_changePw.php'">
                             <!-- login_info -->
