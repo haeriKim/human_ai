24cd45023627392dd0108c24f6403d3ddd328a6d
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-10-18 11:16:30 +0900

    서진웅 // "CCE" → "The Alls"

--- coin/login_c_changePw.php
+++ coin/login_c_changePw.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
-    <title>CCE</title>
+    <title>The Alls</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/login_c_changePw.css">
     <link rel="stylesheet" href="css/common.css">
