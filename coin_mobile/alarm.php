053b30943f0aed666b9d8e5f50daa90a78de9add
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-19 22:04:59 +0900

    전현석 // 모바일js, alarm페이지

---
+++ coin_mobile/alarm.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/exchange66.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header">
+              <h2 class="header_title">알림</h2>
+              <div class="alarm_area">
+                <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- content_wrap -->
+            <div class="content_wrap">
+                <!-- main_content -->
+                <div class="content">
+                    <div class="box" id='deal_orgin_transaction'>
+                        <ul class='market_name'>
+                            <li class='on'><a href="#">전체</a></li>
+                        </ul>
+
+
+
+
+                    </div><!-- con_410 End-->
+                </div><!-- exchange_content End-->
+            </div><!-- content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
--- coin_mobile/js/common.js
+++ coin_mobile/js/common.js
@@ -10,7 +10,7 @@ $(function () {
     $("table.tabs td").click(function () {
         $("table.tabs td span").removeClass("tab_active");
         //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
-        $(this).addClass("tab_active");
+        $(this).addClass("tab_active").css({border:"1px sold #30afb8", color:"#30afb8"});
         $(".tab_content").hide()
         var activeTab = $(this).attr("rel");
         $("#" + activeTab).fadeIn()
