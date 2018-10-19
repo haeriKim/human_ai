747079c0e77eca9fb96cbb1d414131f28670ef6a
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-19 21:02:55 +0900

    전현석 // 거래소 푸터 수정

--- coin/footer.php
+++ coin/footer.php
@@ -2,8 +2,8 @@
 <div id="footer_wrap">
     <div id="footer">
       <div class="compnay_info">
-          <span>㈜필립스멀티<br/></span>
-          <p>서울특별시 송파구 백제고분로 293, 제우빌딩&nbsp;&nbsp; | &nbsp;&nbsp;대표 손용현&nbsp;&nbsp; | &nbsp;&nbsp;사업자등록번호 834-81-01022&nbsp;&nbsp; | &nbsp;&nbsp;통신판매업 제 2018-서울송파-1893호</p>
+          <span>㈜디올스<br/></span>
+          <p>서울특별시 강남구 테헤란로25길 20, 719호(역삼동, 역삼현대벤쳐텔)&nbsp;&nbsp; | &nbsp;&nbsp;대표 손용현&nbsp;&nbsp; | &nbsp;&nbsp;사업자등록번호 492-81-01041&nbsp;&nbsp; | &nbsp;&nbsp;통신판매업 제 2018-서울송파-1893호</p>
           <p>Copyright@All rights reserved.</p>
       </div>
       <div class="footer_menu">
