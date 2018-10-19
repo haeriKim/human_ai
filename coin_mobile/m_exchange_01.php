f57ba2d0594272d33de16ba004eec57ae6b16ce3
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-10-19 10:56:27 +0900

    서진웅 // 모바일 거래소 메인 제작

---
+++ coin_mobile/m_exchange_01.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/exchange.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+            <!-- content_wrap -->
+            <div class="content_wrap">
+                <!-- main_content -->
+                <div class="content exchange_content con_1290">
+                    <div class="box" id='deal_orgin_transaction'>
+                        <table class='deal_table'>
+                          <colgroup>
+                              <col style="width:33%;"/>
+                              <col style="width:23%;"/>
+                              <col style="width:22%;"/>
+                              <col style="width:22%;"/>
+                          </colgroup>
+                          <thead class='thead_deco'>
+                              <tr>
+                                  <th class='coin_kor'>
+                                    <a class="coinName">
+                                      <span>한글명</span>
+                                      <img src="img/side_arrow.png" alt="화살표" class="change_arrow">
+                                    </a>
+                                  </th>
+                                  <th><a>현재가<img src="img/up_down_default.png" alt="화살표"></a></th>
+                                  <th><a>전일대비<img src="img/up_down_down.png" alt="화살표"></a></th>
+                                  <th><a>거래대금<img src="img/up_down_up.png" alt="화살표"></a></th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr>
+                                  <td>
+                                      <p class="kor_name">비트코인</p>
+                                      <p style='display:none;' class="eng_name">Bitcoin</p>
+                                      <span class='unit'>BTC/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_up'>7,500,000</span></td>
+                                  <td><span class='up_down_data money_up'>+1.20%</span></td>
+                                  <td><span class='up_down_data'>74,000,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">비트코인캐시</p>
+                                      <p style='display:none;' class="eng_name">Bitcoin Cash</p>
+                                      <span class='unit'>BCH/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_up'>599,000</span></td>
+                                  <td><span class='up_down_data money_up'>+0.00%</span></td>
+                                  <td><span class='up_down_data'>7,000,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">이더리움</p>
+                                      <p style='display:none;' class="eng_name">Ethereum</p>
+                                      <span class='unit'>ETH/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_up'>800,000</span></td>
+                                  <td><span class='up_down_data money_up'>+1.20%</span></td>
+                                  <td><span class='up_down_data'>700,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">이더리움클래식</p>
+                                      <p style='display:none;' class="eng_name">Ethereum Classic</p>
+                                      <span class='unit'>ETC/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_nochange'>800,000</span></td>
+                                  <td><span class='up_down_data'>+0.00%</span></td>
+                                  <td><span class='up_down_data'>700,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">리플</p>
+                                      <p style='display:none;' class="eng_name">Ripple</p>
+                                      <span class='unit'>XRP/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_down'>900,000</span></td>
+                                  <td><span class='up_down_data money_down'>-1.20%</span></td>
+                                  <td><span class='up_down_data'>700,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">에이다</p>
+                                      <p style='display:none;' class="eng_name">Ada</p>
+                                      <span class='unit'>ADA/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_down'>7,500,000</span></td>
+                                  <td><span class='up_down_data money_down'>-1.20%</span></td>
+                                  <td><span class='up_down_data'>700,000</span></td>
+                              </tr>
+                              <tr>
+                                  <td class='coin_kor'>
+                                      <p class="kor_name">이오스</p>
+                                      <p style='display:none;' class="eng_name">EOS</p>
+                                      <span class='unit'>EOS/KRW</span>
+                                  </td>
+                                  <td><span class='up_down_data money_down'>600,000</span></td>
+                                  <td><span class='up_down_data money_down'>-1.20%</span></td>
+                                  <td><span class='up_down_data'>700,000</span></td>
+                              </tr>
+                          </tbody>
+                        </table>
+                    </div><!-- con_410 End-->
+                </div><!-- exchange_content End-->
+            </div><!-- content_wrap End-->
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <?php include "nav.php";?>
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
--- coin_mobile/nav.php
+++ coin_mobile/nav.php
@@ -2,82 +2,11 @@
 <div id="nav_wrap">
     <!-- nav  -->
     <div id="nav">
-        <h1><a href="index.php"><img src='img/logo.png' alt='로고'></a></h1>
-
-        <div class="after_login login_info">
-            <p>
-                <span class="user_mail">abc@naver.com</span>님
-            </p>
-            <div class="alarmArea">
-              <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
-              <span class="alarmNum alarm_count"></span>
-              <div class="alarm_desc box">
-                  <dl>
-                      <dt>
-                        <h2>알림 (<span class="alarm_count"></span>)</h2>
-                      </dt>
-                      <dd>
-                        <span class="alarmType alarm_r">입금</span>
-                        <a href="" >master678님이 회원님에게 300BTC를 입금하셨습니다.</a>
-                        <span class="alarmTime">5초전</span>
-                        <input type="button" class="delete"/>
-                      </dd>
-                      <dd>
-                        <span class="alarmType alarm_b">출금</span>
-                        <a href="" >100,0000KRW를 출금되었습니다.</a>
-                        <span class="alarmTime">15분전</span>
-                          <input type="button" class="delete"/>
-                      </dd>
-                      <dd>
-                        <span class="alarmType alarm_r">매수</span>
-                        <a href="" >500BTC를 매수하였습니다.</a>
-                        <span class="alarmTime">30분전</span>
-                          <input type="button" class="delete"/>
-                      </dd>
-                      <dd>
-                        <span class="alarmType alarm_b">출금</span>
-                        <a href="" >300BTC가 출금되었습니다.</a>
-                        <span class="alarmTime">1시간전</span>
-                          <input type="button" class="delete"/>
-                      </dd>
-                      <dd>
-                        <span class="alarmType alarm_b">매도</span>
-                        <a href="" >master678님이 회원님에게 300BTC를 송금하셨습니다.</a>
-                        <span class="alarmTime">5시간전</span>
-                        <input type="button" class="delete"/>
-                      </dd>
-                      <dd>
-                        <span class="alarmType alarm_r">매수</span>
-                        <a href="" >master678님이 회원님에게 300BTC를 송금하셨습니다.</a>
-                        <span class="alarmTime">1일전</span>
-                        <input type="button" class="delete"/>
-                      </dd>
-                  </dl>
-                  <a class="alarAllBtn" href="alarm.php" title="알림전체보기">알림전체보기</a>
-              </div>
-            </div>
-        </div>
-
-        <ul class='nav_bt'>
-            <!-- <li class="before_login">
-              <a href='login.php' class="login_bt">로그인</a>
-              <span>|</span>
-              <a href="signUp.php" class="join_bt">회원가입</a>
-            </li> -->
-            <li class="after_login">
-              <a href='myinfo.php' class="login_bt">마이페이지</a>
-              <span>|</span>
-              <a href="#" class="join_bt">로그아웃</a>
-            </li>
-        </ul>
-
         <ul class='nav_list'>
-            <li><a href='exchange.php'>거래소</a></li>
-            <li><a href='mycoin_sub.php'>자산관리</a></li>
-            <li><a href='coin_info_bitcoin.php'>코인정보</a></li>
-            <li><a href='notice.php'>공지사항</a></li>
-            <li><a href='customer_faq.php'>고객지원</a></li>
-
+            <li><a href='#'>거래소</a></li>
+            <li><a href='#'>입출금</a></li>
+            <li><a href='#'>자산관리</a></li>
+            <li><a href='#'>더보기</a></li>
         </ul>
     </div>  <!-- nav End-->
 </div><!-- nav_wrap End-->
