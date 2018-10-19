098a41f0422164e4680550d6fa1c9b55a4af141d
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-19 19:05:42 +0900

    전현석 // 모바일 거래소 호가

---
+++ coin_mobile/m_exchange_05.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/exchange55.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header">
+              <h2 class="header_title">호가</h2>
+              <div class="alarm_area">
+                <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- content_wrap -->
+            <div class="content_wrap">
+                <!-- main_content -->
+                <div class="content">
+                    <div class="box" id='deal_orgin_transaction'>
+                        <ul class='market_name'>
+                            <li class='on'><a href="#">KRW</a></li>
+                        </ul>
+                        <!--bid_price-->
+                        <div class="box bid_price">
+                          <table>
+
+                              <thead class='thead_deco'>
+                                <tr>
+                                  <th class="mado_amount">매도잔량</th>
+                                  <th colspan="2" class="order_price">주문가</th>
+                                  <th colspan="2" class="masu_amount">매수잔량</th>
+                                </tr>
+                              </thead>
+
+                              <tbody>
+                               <tr>
+                                 <td class="mado_right mado1"><div></div>0.100</td>
+                                 <td class="orderbg1 upprice">7,000,000</td>
+                                 <td class="orderbg1 upprice">+3.58%</td>
+                                 <td>전일가격</td>
+                                 <td class="center">7,510,000</td>
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado2"><div></div>0.100</td>
+                                 <td class="orderbg1 upprice">7,000,000</td>
+                                 <td class="orderbg1 upprice">+3.58%</td>
+                                 <td rowspan="2">고가</td>
+
+                                 <td rowspan="2" class="high upprice">7,624,000 <p class="high upprice">(+3.58%)</p></td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado3"><div></div>1.001</td>
+                                 <td class="orderbg1 upprice">7,000,000</td>
+                                 <td class="orderbg1 upprice">+3.58%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado4"><div></div>0.024</td>
+                                 <td class="orderbg1 upprice">7,000,000</td>
+                                 <td class="orderbg1 upprice">+3.58%</td>
+                                 <td rowspan="2">저가</td>
+
+                                 <td rowspan="2" class="low downprice">7,200,000 <p class="high downprice">(-4.13%)</p></td>
+
+
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado5"><div></div>0.267</td>
+                                 <td class="orderbg1">7,000,000</td>
+                                 <td class="orderbg1">0%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado6"><div></div>0.346</td>
+                                 <td class="orderbg1">7,000,000</td>
+                                 <td class="orderbg1">0%</td>
+                                 <td colspan="2" rowspan="5"></td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado7"><div></div>1.000</td>
+                                 <td class="orderbg1">7,000,000</td>
+                                 <td class="orderbg1">0%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado8"><div></div>0.874</td>
+                                 <td class="orderbg1 downprice">7,000,000</td>
+                                 <td class="orderbg1 downprice">-0.21%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado9"><div></div>0.108</td>
+                                 <td class="orderbg1 downprice">7,000,000</td>
+                                 <td class="orderbg1 downprice">-0.21%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td class="mado_right mado10"><div></div>0.108</td>
+                                 <td class="orderbg1 downprice">7,000,000</td>
+                                 <td class="orderbg1 downprice">-0.21%</td>
+
+                               </tr>
+
+                               <tr>
+                                 <td rowspan="10"></td>
+                                 <td class="orderbg2 upprice">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu1 upprice"><div></div>0.100</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 upprice">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu2 upprice"><div></div>0.100</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 upprice">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu3 upprice"><div></div>1.001</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 upprice">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu4 upprice"><div></div>0.024</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu5"><div></div>0.267</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu6"><div></div>0.346</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2">7,000,000</td>
+                                 <td class="orderbg2">+3.58%</td>
+                                 <td colspan="2" class="masu_left masu7"><div></div>1.000</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 downprice">7,000,000</td>
+                                 <td class="orderbg2">-0.21%</td>
+                                 <td colspan="2" class="masu_left masu8 downprice"><div></div>0.874</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 downprice">7,000,000</td>
+                                 <td class="orderbg2">-0.21%</td>
+                                 <td colspan="2" class="masu_left masu9 downprice"><div></div>0.108</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="orderbg2 downprice">7,000,000</td>
+                                 <td class="orderbg2">-0.21%</td>
+                                 <td colspan="2" class="masu_left masu10 downprice"><div></div>0.108</td>
+
+                               </tr>
+
+                               <tr>
+
+                                 <td class="mado_right">13,600</td>
+                                 <td colspan="2" class="all_amount"><b>표시잔량합계</b></td>
+                                 <td colspan="2" class="masu_left">8,000</td>
+
+                               </tr>
+
+
+                              </tbody>
+
+
+                          </table>
+
+                        </div><!--bid_price End-->
+
+                    </div><!-- con_410 End-->
+                </div><!-- exchange_content End-->
+            </div><!-- content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
