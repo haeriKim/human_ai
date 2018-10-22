deaf719880d13a058da23e460f6f3d8c987614f9
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-10-22 17:23:00 +0900

    서진웅 // 거래내역(미완성)

--- coin_mobile/m_mycoin_deal_list.php
+++ coin_mobile/m_mycoin_deal_list.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/mycoin_exit.css">
+    <link rel="stylesheet" href="css/mycoin_deal_list.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/common.js"></script>
@@ -19,7 +19,7 @@
     <div id="wrap">
         <!-- container -->
         <div id="container">
-
+          <!-- header_wrap -->
           <div id ="header_wrap">
             <!-- header -->
             <div id="header">
@@ -32,34 +32,90 @@
             </div> <!-- header End-->
           </div><!-- header_wrap End-->
 
-            <!-- content_wrap -->
+            <!-- mycoin_content_wrap -->
             <div class="mycoin_content_wrap">
 
               <ul class="gnb_tab2">
-                <li class="on"><a href="">자산현황</a></li>
-                <li><a href="">거래내역</a></li>
+                <li><a href="">자산현황</a></li>
+                <li class="on"><a href="#">거래내역</a></li>
                 <li><a href="">입출금</a></li>
-
               </ul>
 
-                <!-- main_content -->
+                <!-- content -->
                 <div class="content">
-                    <div class="box" id='deal_orgin_transaction'>
+                    <ul class='deal_tab'>
+                        <li class='active' rel='deal_complete'>체결</li>
+                        <li class='' rel='deal_not_complete'>미체결</li>
+                    </ul>
+                    <div class="" id='deal_complete'>
+                        <table class='deal_complete_table'>
+                            <thead>
+                                <tr>
+                                    <th>거래시간</th>
+                                    <th>코인</th>
+                                    <th>종류</th>
+                                    <th>거래수량</th>
+                                </tr>
+                                <tr>
+                                    <th>거래단가</th>
+                                    <th>거래금액</th>
+                                    <th>수수료</th>
+                                    <th>정산금액</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class='time'>
+                                        2018-09-05<br>11:23:45
+                                    </td>
+                                    <td>비트코인</td>
+                                    <td class='apply_buy'>매수</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                </tr>
+                            </tbody>
+
+                            <tbody>
+                                <tr>
+                                    <td class='time'>
+                                        2018-08-31<br>08:47:26
+                                    </td>
+                                    <td>이더리움</td>
+                                    <td class='apply_buy'>매도</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                    <td>0KRW</td>
+                                </tr>
+                            </tbody>
+                        </table>
 
 
 
+                    </div>
+                    <div class="" id='deal_not_complete'>
+                    미체결
 
-                    </div><!-- con_410 End-->
-                </div><!-- exchange_content End-->
-            </div><!-- content_wrap End-->
+
+
+                    </div>
+
+                </div><!-- content End-->
+            </div><!-- mycoin_content_wrap End-->
 
             <!-- nav -->
             <?php include "nav.php";?>
             <!-- nav End-->
 
         </div><!-- container End -->
-        <!-- nav_wrap -->
-        <!-- nav_wrap End-->
     </div><!-- wrap End -->
 </body>
 </html>
