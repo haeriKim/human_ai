7ec359aaf2a30204647f167ea03935c13693b805
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-22 09:21:21 +0900

    전현석 // 모바일 거래소

--- coin_mobile/m_exchange_06_06.php
+++ coin_mobile/m_exchange_06_06.php
@@ -23,21 +23,51 @@
           <div id ="header_wrap">
             <!-- header -->
             <div id="header">
-              <h2 class="header_title">호가</h2>
-              <div class="alarm_area">
-                <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+              <div class="headerTop">
+                <input type="button" class="back"/>
+                <!-- 코인선택 영역 -->
+                <div class="coinSelect_area">
+                  <select id="coin_select" name="coin_select">
+                      <option value="전체">비트코인 BTC/KRW</option>
+                      <option value="비트코인캐시 BCH/KRW">비트코인캐시 BCH/KRW</option>
+                      <option value="이더리움 ETH/KRW">이더리움 ETH/KRW</option>
+                      <option value="이더리움클래식 ETC/KRW">이더리움클래식 ETC/KRW</option>
+                      <option value="이오스 EOS/KRW">이오스 EOS/KRW</option>
+                      <option value="리플 XRP/KRW">리플 XRP/KRW</option>
+                      <option value="에이다 ADA/KRW">에이다 ADA/KRW</option>
+                  </select>
+                </div>
+                <div class="alarm_area">
+                  <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+                </div>
               </div>
-            </div> <!-- header End-->
+              <div class="current_price">
+                <div class="price">
+                  <p>현재가</p>
+                  <span><strong>10,650</strong>KRW</span>
+                </div>
+                <div class="percent">
+                  <p>전일대비</p>
+                  <span><strong>-0.31</strong>%</span>
+                  <span><img src="img/arrow_down_blue.png" alt="시세하락">500</span>
+                </div>
+              </div>
           </div><!-- header_wrap End-->
 
             <!-- content_wrap -->
-            <div class="content_wrap">
+            <div class="content_wrap content_wrap_margin">
+
+              <ul class="gnb_tab">
+                <li><a href="">주문</a></li>
+                <li><a href="">호가</a></li>
+                <li><a href="">차트</a></li>
+                <li><a href="">전체체결</a></li>
+                <li class="on"><a href="">코인정보</a></li>
+              </ul>
+
                 <!-- main_content -->
                 <div class="content">
                     <div class="box" id='deal_orgin_transaction'>
-                        <ul class='market_name'>
-                            <li class='on'><a href="#">코인정보</a></li>
-                        </ul>
 
                         <!--coin_detail-->
                         <div id="coin_detail">
@@ -49,17 +79,9 @@
                             <ul>
                               <li>
                                   <strong>리플</strong>
-                                  <b class="coin_word">XRP/KRW</b>
+                                  <b class="coin_word">Reaple</b>
                               </li>
-                              <li>
-                                <span class="price">7,400,000<b class="won">KRW</b>
-                                </span>
-                              </li>
-                              <li class="coin_prepare">
-                                <b>전일대비</b>
-                                <span class="prepare"><b>+0.31</b>%</span>
-                                <span class="updownarrow"><img src="img/arrow_up_red.png" class="ud">23,000</span>
-                              </li>
+
                             </ul>
                           </div>
 
