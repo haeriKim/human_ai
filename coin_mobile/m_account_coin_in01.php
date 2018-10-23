6e7decfb0046368c137ce4a18f8afe5b1e74b7d4
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-10-23 20:51:48 +0900

    전현석 // 코인 입금1 ,출금 1

---
+++ coin_mobile/m_account_coin_in01.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/account_coin.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header2">
+              <div class="headerTop">
+                <input type="button" class="back" onclick="location.href='m_account_main.php'"/>
+                <h2 class="header_title">비트코인 입금 (1/3)</h2>
+                <div class="alarm_area">
+                  <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+                </div>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- mycoin_content_wrap -->
+            <div class="mycoin_content_wrap">
+                <!-- m_mycoin_main -->
+                <div class="content">
+                    <div class="account_area my_assets my_assets_coin01">
+                      <div class="box">
+                      <div class="assetBox">
+                        <ul class="asset all_assets">
+                          <li class='border_bottom_none'>
+                              <p class='mywallet_title'>총 보유자산</p>
+                              <p class="up_down_data mywallet_amount">8,123.60<span>KRW</span></p>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_01'>거래대기</p>
+                                  <p class='notification notification_a_01'>2,992.99 KRW</p>
+                              </div>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_02'>출금가능</p>
+                                  <p class='notification notification_a_02'>381.02 KRW</p>
+                              </div>
+                          </li>
+                          <li class='button_li'>
+                              <a href="#">입금하기</a>
+                              <a href="#">출금하기</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+
+
+                      <div class="coin_in">
+                        <div class="qr_code">
+                          <img src="img/qr_code.png" alt="qr코드">
+                        </div>
+
+                        <p>입금할 BTC 주소(입금전용)</p>
+                        <table class="coin_in_table">
+                          <tbody>
+
+                            <tr>
+                              <td>ewercwsd02s8rch&sm=top_hty&fbm=18468654564=</td>
+                            </tr>
+
+                          </tbody>
+
+                        </table>
+
+                        <div class="copy_bt">
+                          <input type="button" value="복사하기">
+                        </div>
+
+
+                        <div class="coin_warning">
+                          <p>입금신청 주의사항</p>
+                          <ul>
+                            <li>KRW를 처음 입금하시는 경우 3영업일(72시간) 동안 KRW 및 암호화폐의 출금이 제한됩니다.</li>
+                            <li>외부 지갑으로부터 위의 주소로 암호화폐를 전송하게되면 회원님의 전자지갑에 반영됩니다.</li>
+                            <li>입금받는 지갑에서 입금내역을 확인하기까지 30분~1시간 정도의 시간이 소요되며, 상황에 따라 지연이 발생할 수 있습니다.</li>
+                            <li>다른 거래소 지갑으로 송금하는 경우, 해당 거래소 정책의 영향을 받을 수 있습니다.</li>
+                            <li>암호화폐는 전송 후 되돌릴 수 없기 때문에 지갑 주소를 반드시 확인하고 전달해 주세요. 지갑 주소 혼동으로 발생하는 문제나 손실에 대해서는 Tha Alls는 책임지지 않습니다.</li>
+                          </ul>
+                        </div>
+                      </div>
+
+                      <div class="coin_bt_area">
+                        <input type="button" name='button' onclick="location.href='m_account_KRW_in02.php'" value="다음" class="next_bt">
+                      </div>
+
+
+                    </div><!-- assets_area End-->
+                </div><!-- m_mycoin_main End-->
+            </div><!-- mycoin_content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
---
+++ coin_mobile/m_account_coin_out01.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/account_coin.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header2">
+              <div class="headerTop">
+                <input type="button" class="back" onclick="location.href='m_account_main.php'"/>
+                <h2 class="header_title">비트코인 출금 (1/3)</h2>
+                <div class="alarm_area">
+                  <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+                </div>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- mycoin_content_wrap -->
+            <div class="mycoin_content_wrap">
+                <!-- m_mycoin_main -->
+                <div class="content">
+                    <div class="account_area my_assets my_assets_coin01">
+                      <div class="box">
+                      <div class="assetBox">
+                        <ul class="asset all_assets">
+                          <li class='border_bottom_none'>
+                              <p class='mywallet_title'>총 보유자산</p>
+                              <p class="up_down_data mywallet_amount">8,123.60<span>KRW</span></p>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_01'>거래대기</p>
+                                  <p class='notification notification_a_01'>2,992.99 KRW</p>
+                              </div>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_02'>출금가능</p>
+                                  <p class='notification notification_a_02'>381.02 KRW</p>
+                              </div>
+                          </li>
+                          <li class='button_li'>
+                              <a href="#">입금하기</a>
+                              <a href="#">출금하기</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+
+
+                      <div class="coin_in">
+                        <p>BTC 출금</p>
+                        <table class="coin_in_table">
+                          <tbody>
+
+                            <tr>
+                              <th>1회 출금한도</th>
+                              <td>1 BTC</td>
+                            </tr>
+
+                            <tr>
+                              <th>잔여한도</th>
+                              <td>3,000 KRW</td>
+                            </tr>
+
+                            <tr>
+                              <th>비트코인 주소</th>
+                              <td>adlkjlqkwasdasdeli@oasdasd1231234</td>
+                            </tr>
+
+                          </tbody>
+
+                        </table>
+
+
+                        <div class="coin_warning">
+                          <p>출금신청 주의사항</p>
+                          <ul>
+                            <li>회원님의 전자지갑으로부터 외부 전자지갑으로 암호화폐를 출금합니다.</li>
+                            <li>각 암호화폐의 입출금 이후 과정은 블록체인 네트워크에서 자동으로 처리 되므로 The Alls에서 별도의 조취를 취할 수 없습니다.</li>
+                            <li>위 사항 미준수 시 환급은 주말/휴일 제외 1주일 이상 걸림을 유의하시기 바랍니다.</li>
+                            <li>실제 전송되는 수량은 총 출금 암호화폐에서 출금수수료를 제외한 수량이 전송됩니다.</li>
+                            <li>암호화폐의 지갑 주소 오기재로 인한 출금의 경우 The Alls에서 책임지지 않습니다. 지갑주소를 반드시 확인하시기 바랍니다.</li>
+                          </ul>
+                        </div>
+                      </div>
+
+                      <div class="coin_bt_area">
+                        <input type="button" name='button' onclick="location.href='m_account_KRW_in02.php'" value="다음" class="next_bt">
+                      </div>
+
+
+                    </div><!-- assets_area End-->
+                </div><!-- m_mycoin_main End-->
+            </div><!-- mycoin_content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
