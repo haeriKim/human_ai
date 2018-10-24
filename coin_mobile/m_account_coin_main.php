57f8af80778df440855517344ce123b3e43698c6
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-10-24 09:25:56 +0900

    서진웅 // 모바일 비트코인 출금 3단계 , 코인메인

---
+++ coin_mobile/m_account_coin_main.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/account_KRW_main.css">
+    <link rel="stylesheet" href="css/account_main.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header2">
+              <div class="headerTop">
+                <input type="button" class="back" onclick="location.href='m_account_main.php'"/>
+                <h2 class="header_sub_title">비트코인(BTC) 입출금</h2>
+                <div class="alarm_area">
+                  <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+                </div>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- mycoin_content_wrap -->
+            <div class="mycoin_content_wrap">
+                <!-- content -->
+                <div class="content">
+                    <!-- my_assets -->
+                    <div class="account_area my_assets">
+                      <!-- box -->
+                      <div class="box">
+                        <!-- assetBox -->
+                        <div class="assetBox">
+                          <ul class="asset all_assets">
+                            <li class='border_bottom_none'>
+                                <p class='mywallet_title'>총 보유자산</p>
+                                <p class="up_down_data mywallet_amount">8,123.60<span>KRW</span></p>
+                                <div class='notification_wrap'>
+                                    <p class='notification notification_01'>거래대기</p>
+                                    <p class='notification notification_a_01'>2,992.99 KRW</p>
+                                </div>
+                                <div class='notification_wrap'>
+                                    <p class='notification notification_02'>출금가능</p>
+                                    <p class='notification notification_a_02'>381.02 KRW</p>
+                                </div>
+                            </li>
+                            <li class='button_li'>
+                                <a href="m_account_KRW_in01.php">입금하기</a>
+                                <a href="m_account_KRW_out01.php">출금하기</a>
+                            </li>
+                          </ul>
+                        </div><!-- assetBox End-->
+                      </div> <!-- box End-->
+                    </div><!-- my_assets End-->
+
+                    <!-- input_output_list -->
+                    <div class="input_output_list">
+                        <ul class='list_ul'>
+                            <li onclick="location.href='m_account_KRW_detail.php'">
+                                <div class="list_li_row">
+                                    <p class='list_condition'>입금 <span class='waiting'>대기중</span></p>
+                                    <p class='list_amount'>10 BTC</p>
+                                </div>
+                                <div class="list_li_row">
+                                    <p class='list_time'>2018-09-06 23:11:11</p>
+                                    <p class='list_charge'>수수료 0 BTC</p>
+                                </div>
+                                <img src="img/seemore_off.png" alt="자세히" width="20px" height="20px">
+                            </li>
+                            <li>
+                                <div class="list_li_row">
+                                    <p class='list_condition'>출금 <span class='waiting'>대기중</span></p>
+                                    <p class='list_amount'>1,000 BTC</p>
+                                </div>
+                                <div class="list_li_row">
+                                    <p class='list_time'>2018-09-05 11:23:45</p>
+                                    <p class='list_charge'>수수료 0 BTC</p>
+                                </div>
+                                <img src="img/seemore_off.png" alt="자세히" width="20px" height="20px">
+                            </li>
+                        </ul>
+                    </div><!-- input_output_list End-->
+
+                </div><!-- content End-->
+            </div><!-- mycoin_content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
---
+++ coin_mobile/m_account_coin_out03.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/account_coin.css">
+    <link rel="stylesheet" href="css/account_coin_out03.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <!-- <script src="js/fouc.js"></script> -->
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <div id ="header_wrap">
+            <!-- header -->
+            <div id="header2">
+              <div class="headerTop">
+                <input type="button" class="back" onclick="location.href='m_account_coin_out02.php'"/>
+                <h2 class="header_title">비트코인(BTC) 출금 (3/3)</h2>
+                <div class="alarm_area">
+                  <img src="img/bell.png" class="alarmBtn" alt="알림버튼"/>
+                </div>
+              </div>
+            </div> <!-- header End-->
+          </div><!-- header_wrap End-->
+
+            <!-- mycoin_content_wrap -->
+            <div class="mycoin_content_wrap">
+                <!-- m_mycoin_main -->
+                <div class="content">
+                    <div class="account_area my_assets my_assets_KRW03">
+                      <div class="box">
+                      <div class="assetBox">
+                        <ul class="asset all_assets">
+                          <li class='border_bottom_none'>
+                              <p class='mywallet_title'>총 보유자산</p>
+                              <p class="up_down_data mywallet_amount">8,123.60<span>KRW</span></p>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_01'>거래대기</p>
+                                  <p class='notification notification_a_01'>2,992.99 KRW</p>
+                              </div>
+                              <div class='notification_wrap'>
+                                  <p class='notification notification_02'>출금가능</p>
+                                  <p class='notification notification_a_02'>381.02 KRW</p>
+                              </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+
+
+                      <div class="KRW_in">
+                        <p>출금 정보</p>
+                        <table class="KRW_in_table">
+                          <tbody>
+
+                            <tr class='coin_address'>
+                              <th>나의 비트코인 주소</th>
+                              <td>3K7pY2JZrvkda5ZN35WjZf79jfqqx4JmRJ</td>
+                            </tr>
+
+                            <tr class='coin_address'>
+                              <th>비트코인 출금 주소</th>
+                              <td>3K7pY2JZrvkda5ZN35WjZf79jfqqx4JmRJ</td>
+                            </tr>
+
+                            <tr>
+                              <th>출금날짜</th>
+                              <td>2018-09-21 17:00:05</td>
+                            </tr>
+
+                            <tr>
+                              <th>출금금액</th>
+                              <td>11,000,000 BTC</td>
+                            </tr>
+
+                          </tbody>
+
+                        </table>
+
+
+                        <div class="KRW_warning">
+
+
+                        </div>
+                      </div>
+
+                      <div class="KRW_bt_area">
+                        <input type="button" name='button' onclick="location.href='m_account_KRW_in04.php'" value="완료" class="next_bt">
+                      </div>
+
+
+                    </div><!-- assets_area End-->
+                </div><!-- m_mycoin_main End-->
+            </div><!-- mycoin_content_wrap End-->
+
+            <!-- nav -->
+            <?php include "nav.php";?>
+            <!-- nav End-->
+
+        </div><!-- container End -->
+        <!-- nav_wrap -->
+        <!-- nav_wrap End-->
+    </div><!-- wrap End -->
+</body>
+</html>
