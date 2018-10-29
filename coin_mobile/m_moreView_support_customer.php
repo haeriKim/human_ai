a314e8cf80bd31dfcf1b108cf1f54b90fe7ac087
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-10-29 10:16:28 +0900

    서진웅 // 모바일 고객지원 페이지

--- coin_mobile/m_moreView_support_customer.php
+++ coin_mobile/m_moreView_support_customer.php
@@ -22,10 +22,11 @@
             <!-- header_wrap -->
             <div id ="header_wrap">
                 <!-- header -->
-                <div id="header">
+                <div id="header2">
                     <!-- headerTop -->
                     <div class="headerTop">
-                        <h2 class="header_title">더보기</h2>
+                        <input type="button" class="back" onclick="location.href='m_moreView_main.php'"/>
+                        <h2 class="header_title">고객지원</h2>
                         <!-- alarm_area -->
                         <div class="alarm_area">
                             <img src="img/bell.png" class="alarmBtn" alt="알림버튼" onclick="location.href='m_alarm.php'"/>
@@ -37,34 +38,20 @@
             <!-- moreView_content_wrap -->
             <div class="moreView_content_wrap">
                 <ul>
-                    <li class='user_info_li'>
-                        <p class='user_mail'>abc@naver.com</p>
-                        <input type="button" name="logout" value="로그아웃" class='log_out waiting' onclick="location.href='m_login.php'">
-                    </li>
-                    <li onclick="location.href='m_moreView_myinfo.php'">
-                        <p>회원정보</p>
-                        <p class='waiting only_pc'>회원정보 관리 및 변경은 웹(PC)에서 가능합니다.</p>
-                        <img src="img/m_gothru.png" alt="자세히" class='gothru gothru_03'>
-                    </li>
-                    <li onclick="location.href='m_certification_main.php'">
-                        <p>인증센터</p>
+                    <li>
+                        <p>FAQ</p>
                         <img src="img/m_gothru.png" alt="자세히" class='gothru'>
                     </li>
-                    <li onclick="location.href='m_moreView_notice.php'">
-                        <p>공지사항</p>
+                    <li>
+                        <p>1:1 문의</p>
                         <img src="img/m_gothru.png" alt="자세히" class='gothru'>
                     </li>
-                    <li onclick="location.href='m_support_customer.php'">
-                        <p>고객지원</p>
-                        <p class='waiting only_pc'>FAQ, 1:1 문의, 인증자료 제출안내, 수수료 안내를 보실 수 있습니다.</p>
-                        <img src="img/m_gothru.png" alt="자세히" class='gothru gothru_02'>
-                    </li>
-                    <li onclick="location.href='m_moreView_track_ip.php'">
-                        <p>접속 이력확인</p>
+                    <li>
+                        <p>인증자료 제출안내</p>
                         <img src="img/m_gothru.png" alt="자세히" class='gothru'>
                     </li>
-                    <li onclick="location.href='m_moreView_tos.php'">
-                        <p>이용약관 / 개인정보처리방침</p>
+                    <li>
+                        <p>수수료 안내</p>
                         <img src="img/m_gothru.png" alt="자세히" class='gothru'>
                     </li>
                 </ul>
