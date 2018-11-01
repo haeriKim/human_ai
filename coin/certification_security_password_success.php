494035229e158d5c79b6089819081d9e93a335e5
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-01 17:51:41 +0900

    전현석 // 링크수정, 사인업 css 수정

--- coin/certification_security_password_success.php
+++ coin/certification_security_password_success.php
@@ -143,7 +143,7 @@
 
                       <div class="cancel_or_apply">
                         <input type="button" name="" value="확인" onclick="location.href='myinfo.php'" class="cancel_btn">
-                        <input type="button" name="" value="인증하러 가기" onclick="location.href='certification.php'" class="agree_btn">
+                        <input type="button" name="" value="인증하러 가기" onclick="location.href='certification_phone.php'" class="agree_btn">
                       </div>
 
 
