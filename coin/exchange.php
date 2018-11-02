03d818c400811b762a17d87819608b7b28caeb68
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-02 11:25:53 +0900

    전현석 // 거래소 알림에러 수정

--- coin/exchange.php
+++ coin/exchange.php
@@ -1148,10 +1148,6 @@
         <!-- container -->
         <div id="container">
 
-          <!-- nav_wrap -->
-          <?php include "nav.php";?>
-                    <!-- nav_wrap End-->
-
           <!-- m_nav-->
           <?php include "m_nav.php";?>
           <!-- m_nav End-->
@@ -1159,11 +1155,6 @@
           <!-- m_header-->
           <?php include "m_header.php";?>
           <!-- m_header End-->
-
-
-          <!-- header_wrap -->
-          <?php include "header.php";?>
-          <!-- header_wrap End-->
 
             <!-- content_wrap -->
             <div class="content_wrap">
