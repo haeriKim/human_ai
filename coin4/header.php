5b1304557fd2b81c7d5d2aeed8669b0c01f867b8
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-11-02 16:10:11 +0900

    서진웅 // faq수정 , 헤더 수정

--- coin4/header.php
+++ coin4/header.php
@@ -7,38 +7,38 @@
           <ul>
             <li class='btc'>
                 <span>비트코인(BTC)</span>
-                <p><span class="current_pri">7,595,000</span> KRW</p>
-                <b class="up"><span class="current_per">+ 3.98</span> %</b>
+                <p><span class="current_pri">7,400,000</span> KRW</p>
+                <b class="down"><span class="current_per">- 0.31</span> %</b>
             </li>
             <li class='bth'>
               <span>비트코인캐시(BCH)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="down"><span class="current_per">- 3.98</span> %</b>
+              <p><span class="current_pri">481,400</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.23</span> %</b>
             </li>
             <li class='eth'>
               <span>이더리움(ETH)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="up"><span class="current_per">+ 3.98</span> %</b>
+              <p><span class="current_pri">224,500</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.22</span> %</b>
             </li>
             <li class='etc'>
               <span>이더리움클래식(ETC)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="down"><span class="current_per">- 3.98</span> %</b>
+              <p><span class="current_pri">10,190</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.20</span> %</b>
             </li>
             <li class='eos'>
               <span>이오스(EOS)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="down"><span class="current_per">- 3.98</span> %</b>
+              <p><span class="current_pri">5,965</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.68</span> %</b>
             </li>
             <li class='xrp'>
               <span>리플(XRP)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="up"><span class="current_per">+ 3.98</span> %</b>
+              <p><span class="current_pri">517</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.19</span> %</b>
             </li>
             <li class='ada'>
               <span>에이다(ADA)</span>
-              <p><span class="current_pri">7,595,000</span> KRW</p>
-              <b class="up"><span class="current_per">+ 3.98</span> %</b>
+              <p><span class="current_pri">79.90</span> KRW</p>
+              <b class="down"><span class="current_per">- 0.12</span> %</b>
             </li>
           </ul>
         </div><!-- current_coin End-->
