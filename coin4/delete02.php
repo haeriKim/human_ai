8500b682e020d327f5bb85a09821a63f88d82627
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-02 16:54:53 +0900

    전현석 // 디테일 2번만

--- coin4/delete02.php
+++ coin4/delete02.php
@@ -54,21 +54,21 @@
                     <!-- 코인선택 영역 -->
                     <div class="coinSelect_area">
                         <dl class="coinSelect">
-                            <dt><a href="#" title="이더리움"><img src="img/eth.png" alt="이더리움"/></a><span>이더리움 ETH/KRW</span>
+                            <dt><img src="img/eth.png" alt="이더리움"/><span>이더리움 ETH/KRW</span>
                               <img src="img/sws-31-sq.png" alt="국기" class="flag"></dt>
-                            <dd><a href="exchange.php" title="비트코인"><img src="img/bitcoin.png" alt="비트코인"/></a><span>비트코인 BTC/KRW</span>
+                            <dd onclick="location.href='exchange.php'"><img src="img/bitcoin.png" alt="비트코인"/><span>비트코인 BTC/KRW</span>
                               <img src="img/usa-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete01.php" title="비트코인캐시"><img src="img/bitcoincash.png" alt="비트코인캐시"/></a><span>비트코인캐시 BCH/KRW</span>
+                            <dd onclick="location.href='delete01.php'"><img src="img/bitcoincash.png" alt="비트코인캐시"/><span>비트코인캐시 BCH/KRW</span>
                               <img src="img/usa-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete02.php" title="이더리움"><img src="img/eth.png" alt="이더리움"/></a><span>이더리움 ETH/KRW</span>
+                            <dd onclick="location.href='delete02.php'"><img src="img/eth.png" alt="이더리움"/><span>이더리움 ETH/KRW</span>
                               <img src="img/sws-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete03.php" title="이더리움클래식"><img src="img/etc.png" alt="이더리움클래식"/></a><span>이더리움클래식 ETC/KRW</span>
+                            <dd onclick="location.href='delete03.php'"><img src="img/etc.png" alt="이더리움클래식"/><span>이더리움클래식 ETC/KRW</span>
                               <img src="img/sws-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete04.php" title="이오스"><img src="img/eos.png" alt="이오스"/></a><span>이오스 EOS/KRW</span>
+                            <dd onclick="location.href='delete04.php'"><img src="img/eos.png" alt="이오스"/><span>이오스 EOS/KRW</span>
                               <img src="img/hk-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete05.php" title="리플"><img src="img/reaple.png" alt="리플"/></a><span>리플 XRP/KRW</span>
+                            <dd onclick="location.href='delete05.php'"><img src="img/reaple.png" alt="리플"/><span>리플 XRP/KRW</span>
                               <img src="img/usa-31-sq.png" alt="국기" class="flag"></dd>
-                            <dd><a href="delete06.php" title="에이다"><img src="img/ada.png" alt="에이다"/></a><span>에이다 ADA/KRW</span>
+                            <dd onclick="location.href='delete06.php'"><img src="img/ada.png" alt="에이다"/><span>에이다 ADA/KRW</span>
                               <img src="img/sws-31-sq.png" alt="국기" class="flag"></dd>
                         </dl>
                     </div>
