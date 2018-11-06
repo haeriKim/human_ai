34feedf87e7d83ef4a0d1b5f705a9390c29f00ce
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-11-06 14:57:22 +0900

    김해리 // 준비중 페이지

---
+++ coin4/preparing.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ko" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script><!--//배너 슬라이더-->
+    <title>The Alls</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/preparing.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/sub_menu.css">
+    <link rel="stylesheet" href="css/alarm.css">
+    <link rel="shortcut icon" href="img/favicon-16.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon-16.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon-32.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon-32.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon-16.png" type="image/x-icon">
+    <link rel="icon" href="img/favicon-16.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon-32.png" type="image/x-icon">
+    <link rel="icon" href="img/favicon-32.png" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/common.js"></script>
+    <script src="js/fouc.js"></script>
+</head>
+<body>
+    <!-- wrap -->
+    <div id="wrap">
+        <!-- container -->
+        <div id="container">
+
+          <!-- nav_wrap -->
+          <?php include "nav.php";?>
+                    <!-- nav_wrap End-->
+
+          <!-- m_nav-->
+          <?php include "m_nav.php";?>
+          <!-- m_nav End-->
+
+          <!-- m_header-->
+          <?php include "m_header.php";?>
+          <!-- m_header End-->
+
+            <!-- content_wrap -->
+            <div class="content_wrap">
+                <!-- main_content -->
+                <div class="content prepare_wrap con_1000">
+                  <h3 class="comming">COMMING SOON</h3>
+                  <div class="contentsArea">
+                    <div class="imgArea">
+                      <img src="img/comingsoon.png" alt="곧오픈이미지"/>
+                    </div>
+                    <div class="textArea">
+                      <p>페이지 준비중입니다.</p>
+                      <strong>이용에 불편을 드려 죄송합니다.</strong>
+                      <strong>빠른 시일내에 서비스할 수 있도록 최선을 다해 노력하겠습니다.</strong>
+                    </div>
+                  </div>
+                </div><!-- prepare_wrap End-->
+            </div><!-- content_wrap End-->
+
+        </div><!-- container End -->
+    </div><!-- wrap End -->
+
+</body>
+</html>
