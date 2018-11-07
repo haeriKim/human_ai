09c3ec1d58f573728451933b1b272983c8b82ee7
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-07 11:43:07 +0900

    전현석 // 회사메인

--- company/index22.php
+++ company/index22.php
@@ -6,6 +6,7 @@
     <title>Human&Ai</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <script src="js/jquery-3.2.1.min.js"></script>
 </head>
@@ -14,10 +15,33 @@
     <!-- 컨테이너 -->
     <div id="container">
       <div class="main_content">
-        
+          <div class="main_text">
+            <p>더 나은 삶을 기대할 수 있는 기술</p>
+            <span><!--Creating Innovation Through Artificial Intelligence Technology-->
+            감성을 담은 인공지능기업 휴먼앤에이아이를 소개합니다.</span>
+          </div>
+
+          <div class="main_nullbox">
+          </div>
+
+          <div class="main_title" onclick="location.href='h_sub_company.php'">
+            <p class="human">H U M A N & A I</p>
+            <p class="solution">S O L U T I O N</p>
+            <div>
+              <span>Technology that can expect a better life</span>
+              <span>Introduce technologies of Human & Ai</span>
+            </div>
+          </div>
 
       </div><!-- main_content End-->
     </div><!-- 컨테이너(container)End -->
 </div><!--wrap-->
+
+<script>
+
+var maintext = $(".main_title")
+maintext.delay(2000).fadeIn(3500);
+
+</script>
 </body>
 </html>
