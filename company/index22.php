4208c47fe6ba51f85b5bee1b13ddd8f3c7fbd677
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-07 11:23:05 +0900

    전현석 // 회사메인

---
+++ company/index22.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=" width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <title>Human&Ai</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <script src="js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+<div id="wrap">
+    <!-- 컨테이너 -->
+    <div id="container">
+      <div class="main_content">
+        
+
+      </div><!-- main_content End-->
+    </div><!-- 컨테이너(container)End -->
+</div><!--wrap-->
+</body>
+</html>
