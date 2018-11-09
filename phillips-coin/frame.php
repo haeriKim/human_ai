d73df3b483e4ccfac3f011f1b230175eaf800045
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-11-09 16:56:52 +0900

    김해리 // 헤더 네비 추가

--- phillips-coin/frame.php
+++ phillips-coin/frame.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/nav.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <script src="js/jquery-3.2.1.min.js"></script>
 </head>
