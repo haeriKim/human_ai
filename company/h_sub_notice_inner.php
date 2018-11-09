49222ee317d7df24d5d329c9da45f85d0899363a
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-09 12:25:36 +0900

    전현석 // 자사 사업분야, 공지사항 내용

--- company/h_sub_notice_inner.php
+++ company/h_sub_notice_inner.php
@@ -44,12 +44,30 @@
                         <span>더 안정적인 서비스를 제공하기 위해 시스템 점검을 실시할 예정입니다. 점검시간 내 일부 서비스 이용이 불가하오니 양해 부탁드립니다.</span><br/>
                         <br/>
                         <span>작업일시</span><br/>
+                        <br/>
                         <span>- 2018.10.30(화) 오후 8시 ~ 10.31(수) 오전 4시(8시간)</span>
                       </div>
 
                       <div class="next_story">
-                        <p><span>이전글<img src="img/arrow_up_blue.png" alt="화살표"></span><span>보안프로그램 SI Security System 출시 연기 안내</span><span>18.09.</span></p>
-                        <p><span>다음글<img src="img/arrow_down_blue.png" alt="화살표"></span><span>Human&Ai 홈페이지 리뉴얼 오픈!</span></p>
+                        <table class="next_story_table">
+                          <tr>
+                            <th class="story_bt">이전글<img src="img/arrow_up_blue.png" alt="화살표"></th>
+                            <td class="story_title">보안프로그램 SI Security System 출시 연기 안내</td>
+                            <td class="story_date">18.09.10</td>
+                          </tr>
+
+                          <tr>
+                            <th class="story_bt">다음글<img src="img/arrow_down_blue.png" alt="화살표"></th>
+                            <td class="story_title">Human&Ai 홈페이지 리뉴얼 오픈!</td>
+                            <td class="story_date">18.11.09</td>
+
+                          </tr>
+
+                        </table>
+
+
+                        <p class="next_story_top"><span class="story_bt">이전글<img src="img/arrow_up_blue.png" alt="화살표"></span><span class="story_title">보안프로그램 SI Security System 출시 연기 안내</span><span>18.09.</span></p>
+                        <p class="next_story_bottom"><span class="story_bt">다음글<img src="img/arrow_down_blue.png" alt="화살표"></span><span class="story_title">Human&Ai 홈페이지 리뉴얼 오픈!</span><span>18.09.</span></p>
                       </div>
 
                     <div class="more_table">
