c2aa48155fb00939bb0b70513bfa63e612733a85
haeriKim <42455799+haeriKim@users.noreply.github.com>
2018-11-09 18:03:16 +0900

    김해리 // index 수정

--- phillips-coin/index.php
+++ phillips-coin/index.php
@@ -20,13 +20,17 @@
         <p><img src="img/logo.png" alt="로고"></p>
 
         <div class="login_input">
-          <input type='text' name="user_id" value="" placeholder="회원아이디" required maxlength="30" minlength="8"  class='user_id' autocomplete="off"/>
-          <input type="password" name="password" value="" placeholder="비밀번호" required maxlength="30" minlength="10" class='user_pass'/>
+          <div class="inputArea idArea">
+            <img src="img/icon_user_b.png" alt="아이디 아이콘"/><input type='text' name="user_id" value="" placeholder="아이디" required maxlength="30" minlength="8"  class='user_id' autocomplete="off"/>
+          </div>
+          <div class="inputArea pwArea">
+            <img src="img/icon_pwd_b.png" alt="패스워드 아이콘"/><input type="password" name="password" value="" placeholder="비밀번호" required maxlength="30" minlength="10" class='user_pass'/>
+          </div>
 
             <div class="login_box">
               <input type="submit" name="login_bt" value="로그인" class="login_bt">
-              <input type="button" name="password_bt" value="비밀번호 찾기" class="password_bt">
-              <input type="button" name="join_bt" value="회원가입" class="join_bt">
+              <input type="button" name="password_bt" value="비밀번호 찾기" class="password_bt" onclick="location.href='pw_forgot.php'">
+              <input type="button" name="join_bt" value="회원가입" class="join_bt" onclick="location.href='signup.php'">
 
               <p class="copyright">CRYPOCURRNCY PILIPS COIN SINCE 2018</p>
             </div>
