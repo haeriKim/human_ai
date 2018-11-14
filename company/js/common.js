e57cbc6a63d8f218f06752cb5294fc4d2e85b859
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-11-14 16:03:16 +0900

    서진웅 // 자사홈피 TOP 버튼 js 수정

--- company/js/common.js
+++ company/js/common.js
@@ -9,6 +9,7 @@
           		});
             $(".top_bt").click(function(){
                 $("html,body").animate({scrollTop:0},500);
+                return false;
                 });
        });
 
