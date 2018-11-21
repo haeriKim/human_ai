9ab6b96f49d9ad143c07d18df65614bcacdec6b2
JeonHyunSeok <42528355+JeonHyunSeok@users.noreply.github.com>
2018-11-21 18:07:14 +0900

    전현석 // 프라임페이지 수정

--- coin4/prime.php
+++ coin4/prime.php
@@ -48,42 +48,114 @@
 
                   <div class="box prime_area">
 
-                      <div class="prime_inner">
+                      <div class="prime_inner"><!--prime_inner-->
 
+                          <div class="prime_main"> <!--프라임메인-->
                               <div class="theallsprime_img">
-                                <p><img src="img/prime_main2.png" alt="거래소프라임메인"></p>
+                                <p><img src="img/prime_main2.png" alt="거래소프라임메인" class="main_img"></p>
+
                               </div>
+                          </div> <!--프라임메인 끝-->
 
-                              <div class="theallsprime_icon">
-                                <p class="prime_text"><span>암호화폐, 블록체인, 핀테크 기술로 디지털 금융 혁신</span>을 주도하는<br/> <b>'디올스의 가치관'</b>은 이렇게 생각합니다.</p>
 
-                                  <div class="theallsprime_icon_img">
+                          <div class="prime_value"><!--프라임벨류-->
+                            <div class="theallsprime_icon">
+                              <p><img src="img/prime_idea.png" alt="프라임아이디어"></p>
+                              <h3>V A L U E</h3>
+                              <p class="prime_text"><span>암호화폐, 블록체인, 핀테크 기술로 디지털 금융 혁신</span>을 주도하는<br/> <b>'디올스의 가치관'</b>은 이렇게 생각합니다.</p>
+                                <!--  <div class="theallsprime_icon_img">
                                     <img src="img/prime_idea.png" alt="프라임이미지">
-                                  </div>
-                              </div>
-
+                                  </div> -->
+                            </div>
                               <div class="theallsprime_number">
-                                <p><img src="img/prime.png" alt="프라임"></p>
+                                <ul>
+                                  <li>
+                                    <img src="img/trust.png" alt="프라임" class="right_img">
+                                    <b>The Alls Value<span> 01</span></b>
+                                    <p>인격에 믿음과 신뢰가 있다.</p>
+                                    <span>There is faith and trust in personality.</span>
+                                  </li>
 
+                                  <li><img src="img/life.png" alt="프라임" class="left_img">
+                                    <b>The Alls Value<span> 02</span></b>
+                                    <p>상대가 살아야 내가 살 수 있다.</p>
+                                    <span>I can live if my opponent lives.</span>
+                                  </li>
+
+                                  <li><img src="img/lnitial.png" alt="프라임" class="right_img">
+                                    <b>The Alls Value<span> 03</span></b>
+                                    <p>초심을 잊어서는 안된다.</p>
+                                    <span>You should not forget your initials.</span>
+                                  </li>
+
+                                  <li><img src="img/think.png" alt="프라임" class="left_img">
+                                    <b>The Alls Value<span> 04</span></b>
+                                    <p>생각은 매일 변화 된다. 그러므로 알아야한다.</p>
+                                    <span>Thoughts change every day. Therefore, we should know.</span>
+                                  </li>
+
+                                  <li><img src="img/development.png" alt="프라임" class="right_img">
+                                    <b>The Alls Value<span> 05</span></b>
+                                    <p>끊임없는 기술 발전에 노력한다.</p>
+                                    <span>We strive for continuous technological development.</span>
+                                  </li>
+
+                                </ul>
                               </div>
-
-                              <div class="theallsprime_icon2">
-                                <p class="prime_text"><b>'디올스 국제 거래소'</b>는 기축통화가 될 수 있도록 만들었다!!!</p>
-
-                                  <div class="theallsprime_icon_img">
-                                    <p><img src="img/prime_ex2.png" alt="프라임이미지"></p>
-                                    <p class="left_text"><strong>타 거래소</strong></p><br/>
-                                    <p>코인제조 → 코인판매 → 상장으로 마무리(전세계 모든 거래소) 특히 보안에 취약함</p>
-
-                                    <p><img src="img/prime_ex1.png" alt="프라임이미지"></p>
-                                    <p class="left_text"><strong>디올스 거래소</strong></p><br/>
-                                    <p>코인제조 → 코인판매 → 디올스 거래소를 통해 온라인 전자상거래가 포인트가 아닌 실시간의 코인가격으로 가능<br/>
-                                      → [QR코드] QR코드를 활용한 오프라인에서도 포인트 아닌 실시간 코인가격으로 사용가능</p>
-                                  </div>
-                              </div>
+                          </div><!--프라임벨류 끝-->
 
 
-                      </div>
+                          <div class="prime_service"><!--프라임서비스-->
+                            <div class="theallsprime_icon2">
+                              <p><img src="img/prime_point.png" alt="프라임아이콘"></p>
+                              <h3>S E R V I C E</h3>
+                              <p class="prime_text">암호화폐의 새로운 기준 <b>'디올스 국제거래소'</b>의 요점은 이렇습니다.</p>
+                              <ul>
+
+                                <li><strong>The Alls Service<b> 01</b></strong><span></span></li>
+                                <li><strong>The Alls Service<b> 02</b></strong><span></span></li>
+                                <li><strong>The Alls Service<b> 03</b></strong><span></span></li>
+                              <!--  <li class="prime_text"><b>'디올스 국제 거래소'</b>는<br/>
+                                </li>
+
+                                <li class="prime_text">기축통화가 될 수 있도록 만들었습니다.</li>
+
+                                <li class="prime_text">+</li>
+
+                                <li class="prime_text">상장되는 모든 코인은 실시간 가격으로 사용이 가능하고,
+                                <br/> 전자상거래 및 QR코드를 활용하여 직접 제품 주인에게 현금이 통장으로 입금됩니다.</li>
+
+                                <li class="prime_text">+</li>
+
+                                <li class="prime_text">상장기반 코인은 디올스,비트코인,비트코인캐시,이더리움,이더리움클래식,에이다,리플,이오스입니다.</li> -->
+                              </ul>
+                            </div>
+
+                            <div class="theallsprime_number2">
+                                <ul>
+                                    <li><img src="img/prime_ex2.png" alt="프라임이미지" class="prime_icon_img">
+
+                                        <p><strong>타 거래소</strong></p>
+                                        <p>코인제조 → 코인판매 → 상장으로 마무리(전세계 모든 거래소) 특히 보안에 취약함</p>
+                                    </li>
+                                  <!--  <p class="left_text"><strong>타 거래소</strong></p><br/>
+                                    <p>코인제조 → 코인판매 → 상장으로 마무리(전세계 모든 거래소) 특히 보안에 취약함</p> -->
+
+                                    <li><img src="img/prime_ex1.png" alt="프라임이미지" class="prime_icon_img">
+
+                                      <p><strong>디올스 거래소</strong></p>
+                                      <p>코인제조 → 코인판매 → 디올스 거래소를 통해 온라인 전자상거래가 포인트가 아닌 실시간의 코인가격으로 가능</p>
+                                    </li>
+                              <!--       <p class="left_text"><strong>디올스 거래소</strong></p><br/>
+                                   <p>코인제조 → 코인판매 → 디올스 거래소를 통해 온라인 전자상거래가 포인트가 아닌 실시간의 코인가격으로 가능<br/>
+                                      → [QR코드] QR코드를 활용한 오프라인에서도 포인트 아닌 실시간 코인가격으로 사용가능</p> -->
+                                </ul>
+                            </div>
+
+                          </div><!--프라임서비스 끝-->
+
+
+                      </div><!--prime_inner 끝-->
 
                   </div><!--coinInfo_area End-->
 
