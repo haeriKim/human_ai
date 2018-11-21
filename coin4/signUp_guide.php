649bb29cf0ead46dea67f959195193ad3f516d9e
seojinwoong <42455534+seojinwoong@users.noreply.github.com>
2018-11-21 17:21:31 +0900

    서진웅 // 거래소 회원가입 가이드 수정 및 반응형작업

--- coin4/signUp_guide.php
+++ coin4/signUp_guide.php
@@ -6,7 +6,6 @@
     <title>The Alls</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/signUp_guide.css">
-    <link rel="stylesheet" href="css/common.css">
     <link rel="shortcut icon" href="img/favicon-16.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon-16.ico" type="image/x-icon">
     <link rel="shortcut icon" href="img/favicon-32.ico" type="image/x-icon">
@@ -37,68 +36,88 @@
 
             <!-- main_content -->
             <div class="main_content">
-
-                <img src="img/profile_pink.png" alt="안내" class='worker_img'>
-                <h2 class='title'>회원가입 안내</h2>
-                <p class='intro_text'>
-                    안녕하세요! 디올스(The Alls)입니다.<br/>
-                    아래 안내에 따라 회원가입을 순서대로 진행해 주시기 바랍니다.
-                </p>
-
+                <div class="intro">
+                    <img src="img/profile_pink.png" alt="안내" class='worker_img'>
+                    <h2 class='title'>회원가입 안내</h2>
+                    <p class='intro_text'>
+                        안녕하세요! 디올스(The Alls)입니다.<br/>
+                        아래 안내에 따라 회원가입을 순서대로 진행해 주시기 바랍니다.
+                    </p>
+                </div>
                 <ul class="step_box">
                     <li class='step_li step_li_01'>
-                        <p class='step_title'>Step 1. 이용약관 동의</p>
-                        <img src="img/guide/01_signup_01.jpg" alt="1단계">
-                        <img src="img/guide/01_signup_01_01.jpg" alt="1-1단계">
+                        <p class='step_title'><span class='step_number'>Step 1.</span> 이용약관 동의</p>
+                        <img src="img/guide/01_signup_01.jpg" alt="image1">
+                        <img src="img/guide/01_signup_3.jpg" alt="image2">
                         <p class='subscription'>
-                            좌측의 이용약관들을 꼼꼼히 읽어보시고 '동의합니다' 부분을 체크해주세요.<br>
-                            약관에 동의를 하셔야 '회원가입' 버튼이 활성화되어 다음 과정을 진행하실 수 있습니다.<br>
-                            (마지막 '이벤트 메세지 수신 동의' 부분은 선택사항입니다.)
+                            좌측의 이용약관들을 꼼꼼히 읽어보시고 <span class='impact'>'동의합니다'</span> 부분을 체크해주세요. 하단의 <span class='impact'>'모두 동의합니다'</span> 부분을 체크하시면 모든 동의항목에 체크가 됩니다.<br>
+                            <span class='impact'>약관에 동의를 하셔야 다음 과정을 진행하실 수 있습니다.</span> (마지막 '이벤트 알림 메세지 수신 동의' 부분은 선택사항입니다.)
                         </p>
                     </li>
                     <li class='step_li step_li_02'>
-                        <p class='step_title'>Step 2. 이메일주소 입력</p>
-                        <img src="img/guide/01_signup_02.jpg" alt="2단계">
+                        <p class='step_title'><span class='step_number'>Step 2.</span> 이메일주소 입력</p>
+                        <img src="img/guide/01_signup_4.jpg" alt="image3">
                         <p class='subscription'>
-                            약관들을 동의하셨으면 거래소에서 이용하실 '이메일주소'를 정확하게 입력해 주세요.<br>
-                            주의하실 점은 기존에 존재하는 이메일주소를 입력하셔야 합니다.<br>
+                            약관들을 동의하셨으면 거래소에서 이용하실 <span class='impact'>'이메일주소'</span>를 정확하게 입력해 주세요.<br>
+                            <span class='impact'>주의하실 점은 기존에 존재하는 이메일주소를 입력하셔야 합니다.</span>
                             (입력하신 이메일로 인증코드가 발송되기 때문에 꼭 존재하는 이메일주소를 입력해 주시기 바랍니다.)
                         </p>
                     </li>
                     <li class='step_li step_li_03'>
-                        <p class='step_title'>Step 3-1. 로그인 비밀번호 입력</p>
-                        <img src="img/guide/01_signup_03.jpg" alt="3단계">
-                        <img src="img/guide/01_signup_03_01.jpg" alt="3-1단계">
+                        <p class='step_title'><span class='step_number'>Step 3-1.</span> 로그인 비밀번호 입력</p>
+                        <img src="img/guide/01_signup_5.jpg" alt="image4">
+                        <img src="img/guide/01_signup_6.jpg" alt="image5">
                         <p class='subscription'>
-                            이메일주소를 입력하신 후, '로그인 비밀번호'를 입력해 주세요.<br>
-                            로그인 비밀번호 입력창에 값을 입력하면 하단에 '비밀번호 충족요건' 창이 자동으로 뜨게 됩니다.
+                            이메일주소를 입력하신 후, <span class='impact'>'로그인 비밀번호'</span>를 입력해 주세요.<br>
+                            로그인 비밀번호 입력창에 값을 입력하면 하단에 <span class='impact'>'비밀번호 충족조건' 창이 자동으로 뜨게 됩니다.</span>
                         </p>
                     </li>
                     <li class='step_li step_li_04'>
-                        <p class='step_title'>Step 3-2. 로그인 비밀번호 요건충족</p>
-                        <img src="img/guide/01_signup_03_02.jpg" alt="3-2단계">
-                        <img src="img/guide/01_signup_03_03.jpg" alt="3-3단계">
+                        <p class='step_title'><span class='step_number'>Step 3-2.</span> 로그인 비밀번호 조건충족</p>
+                        <img src="img/guide/01_signup_7.jpg" alt="image6">
+                        <img src="img/guide/01_signup_8.jpg" alt="image7">
                         <p class='subscription'>
-                            비밀번호 요건이 충족되면 해당조건 부분 글씨가 남색으로 변하면서 체크가 되며,<br>
+                            비밀번호 조건이 충족되면 해당조건 부분 글씨가 남색으로 변하면서 체크가 되며,
                             충족되지 않은 부분은 계속해서 빨간색으로 표시가 됩니다.<br>
-                            모든 충족요건에 맞는 비밀번호를 입력하셔야 회원가입이 가능합니다.
+                            <span class='impact'>모든 충족조건에 맞는 비밀번호를 입력하셔야 회원가입이 가능합니다.</span>
                         </p>
                     </li>
                     <li class='step_li step_li_05'>
-                        <p class='step_title'>Step 4. 이메일 인증</p>
-                        <img src="img/guide/01_signup_04.jpg" alt="4단계">
-                        <img src="img/guide/01_signup_04_01.jpg" alt="4-1단계">
+                        <p class='step_title'><span class='step_number'>Step 4-1.</span> 이메일 인증</p>
+                        <img src="img/guide/01_signup_9.jpg" alt="image8">
+                        <img src="img/guide/01_signup_10.jpg" alt="image9">
                         <p class='subscription'>
-                            이메일주소와 로그인 비밀번호를 입력하신 후 '회원가입' 버튼을 누르면 '이메일 인증' 팝업창이 뜨게됩니다.<br>
-                            입력하신 이메일주소로 인증코드가 담긴 메일이 발송됩니다. 메일을 확인하신 후 인증코드를 입력하시고 하단에 '이메일인증' 버튼을 눌러주세요.<br>
-                            인증코드가 담긴 메일을 받지 못하셨거나, 입력시간이 만료된 경우에는 '인증코드 재전송' 버튼을 눌러 새로운 인증코드를 발급받으시길 바랍니다.
+                            이메일주소와 로그인 비밀번호를 조건에 맞게 입력하시면 <span class='impact'>'회원가입' 버튼이 활성화됩니다.</span><br>
+                            버튼을 누르시면 <span class='impact'>'이메일 인증'</span> 팝업이 나타나고 <span class='impact'>입력하신 이메일 주소로 인증코드가 담긴 메일이 발송됩니다.</span>
                         </p>
                     </li>
                     <li class='step_li step_li_06'>
-                        <p class='step_title'>Step 5. 회원가입 완료</p>
-                        <img src="img/guide/01_signup_05.jpg" alt="5단계">
+                        <p class='step_title'><span class='step_number'>Step 4-2.</span> 이메일 인증코드 입력</p>
+                        <img src="img/guide/01_signup_email.jpg" alt="image10">
+                        <img src="img/guide/01_signup_11.jpg" alt="image11">
                         <p class='subscription'>
-                            이메일 인증까지 마치시면 회원가입이 완료됩니다. 회원님의 인증단계는 <span class='level_01'>Level 1</span>이 되며, Level 2(보안비밀번호 생성)단계를 바로 인증하실 수 있습니다.<br>
+                            메일을 확인하신 후 인증코드 입력란에 <span class='impact'>'인증번호'</span>를 정확하게 입력해 주세요.
+                        </p>
+                    </li>
+                    <li class='step_li step_li_07'>
+                        <p class='step_title'><span class='step_number'>Step 4-3.</span> 이메일 인증코드 재전송</p>
+                        <img src="img/guide/01_signup_13.jpg" alt="image12">
+                        <p class='subscription'>
+                            만약 인증코드가 담긴 메일을 받지 못하셨거나, 입력시간이 만료된 경우에는 <span class='impact'>'인증코드 재전송'</span> 버튼을 눌러 새로운 인증코드를 발급받으시길 바랍니다.
+                        </p>
+                    </li>
+                    <li class='step_li step_li_08'>
+                        <p class='step_title'><span class='step_number'>Step 4-4.</span> 이메일 인증완료</p>
+                        <img src="img/guide/01_signup_12.jpg" alt="image13">
+                        <p class='subscription'>
+                            '인증코드'를 입력하신 후 <span class='impact'>'이메일인증'</span> 버튼을 누르면 이메일 인증이 완료됩니다.
+                        </p>
+                    </li>
+                    <li class='step_li step_li_09'>
+                        <p class='step_title'><span class='step_number'>Step 5.</span> 회원가입 완료</p>
+                        <img src="img/guide/01_signup_14.jpg" alt="image14">
+                        <p class='subscription'>
+                            이메일 인증까지 마치시면 회원가입이 완료됩니다. 회원님의 인증단계는 <span class='impact'>Level 1</span>이 되며, Level 2(보안비밀번호 생성)단계를 바로 인증하실 수 있습니다.<br>
                             하단에 '로그인 하기' 버튼을 누르시면 로그인 창으로 이동하게 됩니다.
                         </p>
                     </li>
