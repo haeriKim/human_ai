d33af078db61863780022de9ec2556e25fbf9470
JeonHyunSeok <ssicago91@naver.com>
2018-11-30 10:15:51 +0900

    전현석 // 헤더 수정

--- coin4/header.php
+++ coin4/header.php
@@ -5,39 +5,81 @@
         <!-- current_coin -->
         <div class="current_coin">
           <ul>
-            <li class='btc'>
+            <li class='btc stock_header'>
               <span>비트코인(BTC)</span>
-              <p><span class="current_pri">7,400,000</span> KRW</p>
+
+              <ul id="stock_01" class="stock">
+                <li><p><span class="current_pri">7,400,000</span> KRW</p></li>
+                <li><p><span class="current_pri">7,456,400</span> KRW</p></li>
+                <li><p><span class="current_pri">7,452,400</span> KRW</p></li>
+              </ul>
+
               <b class="up"><span class="current_per">+ 0.31</span> %</b>
             </li>
-            <li class='bth'>
+            <li class='bth stock_header'>
               <span>비트코인캐시(BCH)</span>
-              <p><span class="current_pri">481,400</span> KRW</p>
+
+              <ul id="stock_02" class="stock">
+                <li><p><span class="current_pri">481,400</span> KRW</p></li>
+                <li><p><span class="current_pri">480,320</span> KRW</p></li>
+                <li><p><span class="current_pri">480,400</span> KRW</p></li>
+              </ul>
+
               <b class="down"><span class="current_per">- 0.23</span> %</b>
             </li>
-            <li class='eth'>
+            <li class='eth stock_header'>
               <span>이더리움(ETH)</span>
-              <p><span class="current_pri">224,500</span> KRW</p>
+
+              <ul id="stock_03" class="stock">
+                <li><p><span class="current_pri">225,570</span> KRW</p></li>
+                <li><p><span class="current_pri">224,500</span> KRW</p></li>
+                <li><p><span class="current_pri">225,400</span> KRW</p></li>
+              </ul>
+
               <b class="up"><span class="current_per">+ 0.22</span> %</b>
             </li>
-            <li class='etc'>
+            <li class='etc stock_header'>
               <span>이더리움클래식(ETC)</span>
-              <p><span class="current_pri">10,190</span> KRW</p>
+
+              <ul id="stock_04" class="stock">
+                <li><p><span class="current_pri">10,190</span> KRW</p></li>
+                <li><p><span class="current_pri">9,990</span> KRW</p></li>
+                <li><p><span class="current_pri">10,020</span> KRW</p></li>
+              </ul>
+
               <b class="down"><span class="current_per">- 0.20</span> %</b>
             </li>
-            <li class='eos'>
+            <li class='eos stock_header'>
               <span>이오스(EOS)</span>
-              <p><span class="current_pri">5,965</span> KRW</p>
+
+              <ul id="stock_05" class="stock">
+                <li><p><span class="current_pri">5,965</span> KRW</p></li>
+                <li><p><span class="current_pri">5,905</span> KRW</p></li>
+                <li><p><span class="current_pri">5,870</span> KRW</p></li>
+              </ul>
+
               <b class="down"><span class="current_per">- 0.68</span> %</b>
             </li>
-            <li class='xrp'>
+            <li class='xrp stock_header'>
               <span>리플(XRP)</span>
-              <p><span class="current_pri">517</span> KRW</p>
+
+              <ul id="stock_06" class="stock">
+                <li><p><span class="current_pri">517</span> KRW</p></li>
+                <li><p><span class="current_pri">515</span> KRW</p></li>
+                <li><p><span class="current_pri">516</span> KRW</p></li>
+              </ul>
+
               <b class="down"><span class="current_per">- 0.19</span> %</b>
             </li>
-            <li class='ada'>
+            <li class='ada stock_header'>
               <span>에이다(ADA)</span>
-              <p><span class="current_pri">79.90</span> KRW</p>
+
+              <ul id="stock_07" class="stock">
+                <li><p><span class="current_pri">79.90</span> KRW</p></li>
+                <li><p><span class="current_pri">80.00</span> KRW</p></li>
+                <li><p><span class="current_pri">79.95</span> KRW</p></li>
+              </ul>
+
               <b class="up"><span class="current_per">+ 0.12</span> %</b>
             </li>
           </ul>
