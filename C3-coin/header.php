347d65d76cca6a6b5f189d0ce2c83fedef976923
JeonHyunSeok <ssicago91@naver.com>
2018-12-03 14:51:03 +0900

    전현석 // c3 수정

--- C3-coin/header.php
+++ C3-coin/header.php
@@ -1,5 +1,5 @@
 <div id="headerWrap">
     <div id="header">
-        <h1><a href="index.php"><img src='img/logo2.png' alt='로고'></a></h1>
+        <h1><a href="index.php"><img src='img/logo3.png' alt='로고'></a></h1>
     </div>
 </div>
