ea6318036ca6755029304c249c5aa027add683b9
JeonHyunSeok <ssicago91@naver.com>
2018-12-04 14:23:21 +0900

    전현석 // 헤더 수정

--- coin4/header.php
+++ coin4/header.php
@@ -9,9 +9,9 @@
               <span>비트코인(BTC)</span>
 
               <ul id="stock_01" class="stock">
-                <li><p><span class="current_pri">7,400,000</span> KRW</p></li>
-                <li><p><span class="current_pri">7,456,400</span> KRW</p></li>
-                <li><p><span class="current_pri">7,452,400</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 7,456,400</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 7,400,000</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 3,225.800</span> BTC</p></li>
               </ul>
 
               <b class="up"><span class="current_per">+ 0.31</span> %</b>
@@ -20,9 +20,9 @@
               <span>비트코인캐시(BCH)</span>
 
               <ul id="stock_02" class="stock">
-                <li><p><span class="current_pri">481,400</span> KRW</p></li>
-                <li><p><span class="current_pri">480,320</span> KRW</p></li>
-                <li><p><span class="current_pri">480,400</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 481,400</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 480,320</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 1,930.800</span> BCH</p></li>
               </ul>
 
               <b class="down"><span class="current_per">- 0.23</span> %</b>
@@ -31,9 +31,9 @@
               <span>이더리움(ETH)</span>
 
               <ul id="stock_03" class="stock">
-                <li><p><span class="current_pri">225,570</span> KRW</p></li>
-                <li><p><span class="current_pri">224,500</span> KRW</p></li>
-                <li><p><span class="current_pri">225,400</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 225,570</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 224,500</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 28,205.483</span> ETH</p></li>
               </ul>
 
               <b class="up"><span class="current_per">+ 0.22</span> %</b>
@@ -42,9 +42,9 @@
               <span>이더리움클래식(ETC)</span>
 
               <ul id="stock_04" class="stock">
-                <li><p><span class="current_pri">10,190</span> KRW</p></li>
-                <li><p><span class="current_pri">9,990</span> KRW</p></li>
-                <li><p><span class="current_pri">10,020</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 10,190</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 9,990</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 42,842.668</span> ETC</p></li>
               </ul>
 
               <b class="down"><span class="current_per">- 0.20</span> %</b>
@@ -53,9 +53,9 @@
               <span>이오스(EOS)</span>
 
               <ul id="stock_05" class="stock">
-                <li><p><span class="current_pri">5,965</span> KRW</p></li>
-                <li><p><span class="current_pri">5,905</span> KRW</p></li>
-                <li><p><span class="current_pri">5,870</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 5,965</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 5,905</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 495,836.923</span> EOS</p></li>
               </ul>
 
               <b class="down"><span class="current_per">- 0.68</span> %</b>
@@ -64,9 +64,9 @@
               <span>리플(XRP)</span>
 
               <ul id="stock_06" class="stock">
-                <li><p><span class="current_pri">517</span> KRW</p></li>
-                <li><p><span class="current_pri">515</span> KRW</p></li>
-                <li><p><span class="current_pri">516</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 517</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 515</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 16,974,321.532</span> XRP</p></li>
               </ul>
 
               <b class="down"><span class="current_per">- 0.19</span> %</b>
@@ -75,9 +75,9 @@
               <span>에이다(ADA)</span>
 
               <ul id="stock_07" class="stock">
-                <li><p><span class="current_pri">79.90</span> KRW</p></li>
-                <li><p><span class="current_pri">80.00</span> KRW</p></li>
-                <li><p><span class="current_pri">79.95</span> KRW</p></li>
+                <li><p><span class="current_pri">고가 80.90</span> KRW</p></li>
+                <li><p><span class="current_pri">저가 79.00</span> KRW</p></li>
+                <li><p><span class="current_pri">거래량 27,973,320.338</span> ADA</p></li>
               </ul>
 
               <b class="up"><span class="current_per">+ 0.12</span> %</b>
