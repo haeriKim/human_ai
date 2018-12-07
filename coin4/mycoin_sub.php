6eb0fcde5f6eb5028317b2ef8c223e5a4b3126c0
JeonHyunSeok <ssicago91@naver.com>
2018-12-07 10:31:14 +0900

    전현석 // app_PC_page 활성화

--- coin4/mycoin_sub.php
+++ coin4/mycoin_sub.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="css/popup.css">
     <link rel="stylesheet" href="css/common_table.css">
     <link rel="stylesheet" href="css/sub_menu.css">
+    <link rel="stylesheet" href="css/app_page_PC.css">
     <link rel="shortcut icon" href="img/favicon-16.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon-16.ico" type="image/x-icon">
     <link rel="shortcut icon" href="img/favicon-32.ico" type="image/x-icon">
@@ -1853,5 +1854,67 @@
                     </div><!-- full_content End-->
             <div id="shadow"></div>
     </div>  <!-- full End-->
+
+    <!-- wrap -->
+    <div id="app_wrap">
+        <!-- container -->
+        <div id="container">
+
+          <!-- nav_wrap -->
+          <?php include "nav.php";?>
+          <!-- nav_wrap End-->
+
+          <!-- m_nav-->
+          <?php include "m_nav.php";?>
+          <!-- m_nav End-->
+
+          <!-- m_header-->
+          <?php include "m_header.php";?>
+          <!-- m_header End-->
+
+
+          <!-- header_wrap -->
+          <?php include "header.php";?>
+          <!-- header_wrap End-->
+
+            <!-- content_wrap -->
+            <div class="content_wrap">
+                <!-- common_table_wrap -->
+                <div class="content common_table_wrap con_1250">
+
+                  <!-- app_page_content -->
+                  <div class="app_pagePC_content">
+
+                    <div class="app_pagePC_story">
+                      <img src="img/exclamation6.png" alt="컴퓨터아이콘">
+                      <p>현재 페이지는 디올스 PC버전을 통해<br/>
+                      이용하실수 있습니다.</p>
+
+                      <div class="app_pagePC_bt">
+                        <input type="button" name="거래소버튼" value="홈으로 돌아가기" onclick="location.href='index.php'" class="app_bt">
+                      </div>
+
+
+
+                    </div>
+
+                  </div><!-- app_page_content End-->
+
+                </div><!-- common_table_wrap End-->
+            </div><!-- content_wrap End-->
+
+            <!-- footer_wrap -->
+            <?php include "footer.php";?>
+            <!-- footer_wrap End-->
+
+            <!--sub_menu-->
+            <?php include "sub_menu.php";?>
+            <!--sub_menu End-->
+
+        </div><!-- container End -->
+    </div><!-- wrap End -->
+
+
+
 </body>
 </html>
